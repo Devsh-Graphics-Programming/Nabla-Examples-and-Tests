6e0bc1daa5bee9971299ea658b57a0f5b4f0a83d
Erfan <ahmadierfan99@gmail.com>
2023-01-02 08:53:47 +0330

    initial commit

---
+++ 62.CAD/main.cpp
@@ -0,0 +1,322 @@
+#define _NBL_STATIC_LIB_
+#include <nabla.h>
+
+#include "../common/CommonAPI.h"
+
+using namespace nbl;
+
+class DrawSystem : public core::IReferenceCounted
+{
+public:
+	DrawSystem(const core::smart_refctd_ptr<nbl::video::ILogicalDevice>& device, uint32_t maxObjects = 128u)
+		: m_device(device)
+		, currentDrawObjectCount(0u)
+	{
+		// Allocate Buffers with maxObjects
+	}
+
+	void Reset()
+	{
+		currentDrawObjectCount = 0u;
+	}
+
+	bool AddPolyline(const std::vector<double>& points)
+	{
+		// currentDrawObjectCount +=
+		// update drawObject Buffer with correct offsets and shit
+		return true;
+	}
+
+	struct UniformData
+	{
+		// 3x3 viewProjection matrix -> rows will be padded to 16 bytes so use 4x4 doubles
+		// screenResolution -> uint32_t = uint2
+		uint32_t lineWidthInPixels = 2u; // later will be moved to a "style" struct with a level of indirection from DrawObject
+	};
+
+	enum ObjectType : uint32_t
+	{
+		OT_LINE = 0u,
+		OT_ELLIPSE,
+	};
+
+	struct DrawObject
+	{
+		ObjectType type; // [0-4)
+		// uint32_t reserved = 0u; // [4-8)
+		// LineData -> Later use BDA -> address of line in polyline position and something to note that if it has prev-next?
+		float nextDir[2]; // [8-16)
+		float prevDir[2]; // [16-24)
+		double start[2]; // [24-40)
+		double end[2]; // [40-56)
+	};
+	static_assert(sizeof(DrawObject) == 56);
+
+	uint32_t currentDrawObjectCount = 0u;
+	core::smart_refctd_ptr<nbl::video::ILogicalDevice> m_device;
+	core::smart_refctd_ptr<video::IGPUBuffer> vertexBuffer; // dvec2's
+	core::smart_refctd_ptr<video::IGPUBuffer> indexBuffer; // uint32_t's
+	core::smart_refctd_ptr<video::IGPUBuffer> objectsBuffer;
+};
+
+class CADApp : public ApplicationBase
+{
+	constexpr static uint32_t FRAMES_IN_FLIGHT = 5u;
+	static constexpr uint64_t MAX_TIMEOUT = 99999999999999ull;
+
+	constexpr static uint32_t WIN_W = 1280u;
+	constexpr static uint32_t WIN_H = 720u;
+
+	core::smart_refctd_ptr<nbl::ui::IWindowManager> windowManager;
+	core::smart_refctd_ptr<nbl::ui::IWindow> window;
+	core::smart_refctd_ptr<CommonAPI::CommonAPIEventCallback> windowCb;
+	core::smart_refctd_ptr<nbl::video::IAPIConnection> apiConnection;
+	core::smart_refctd_ptr<nbl::video::ISurface> surface;
+	core::smart_refctd_ptr<nbl::video::IUtilities> utilities;
+	core::smart_refctd_ptr<nbl::video::ILogicalDevice> logicalDevice;
+	video::IPhysicalDevice* physicalDevice;
+	std::array<video::IGPUQueue*, CommonAPI::InitOutput::MaxQueuesCount> queues;
+	core::smart_refctd_ptr<nbl::video::ISwapchain> swapchain;
+	core::smart_refctd_ptr<nbl::video::IGPURenderpass> renderpass;
+	nbl::core::smart_refctd_dynamic_array<nbl::core::smart_refctd_ptr<nbl::video::IGPUFramebuffer>> framebuffersDynArraySmartPtr;
+	std::array<std::array<nbl::core::smart_refctd_ptr<nbl::video::IGPUCommandPool>, CommonAPI::InitOutput::MaxFramesInFlight>, CommonAPI::InitOutput::MaxQueuesCount> commandPools;
+	core::smart_refctd_ptr<nbl::system::ISystem> system;
+	core::smart_refctd_ptr<nbl::asset::IAssetManager> assetManager;
+	video::IGPUObjectFromAssetConverter::SParams cpu2gpuParams;
+	core::smart_refctd_ptr<nbl::system::ILogger> logger;
+	core::smart_refctd_ptr<CommonAPI::InputSystem> inputSystem;
+	video::IGPUObjectFromAssetConverter cpu2gpu;
+	core::smart_refctd_dynamic_array<core::smart_refctd_ptr<video::IGPUImage>> m_swapchainImages;
+
+	int32_t m_resourceIx = -1;
+
+	core::smart_refctd_ptr<video::IGPUSemaphore> m_imageAcquire[FRAMES_IN_FLIGHT] = { nullptr };
+	core::smart_refctd_ptr<video::IGPUSemaphore> m_renderFinished[FRAMES_IN_FLIGHT] = { nullptr };
+	core::smart_refctd_ptr<video::IGPUFence> m_frameComplete[FRAMES_IN_FLIGHT] = { nullptr };
+
+	core::smart_refctd_ptr<video::IGPUCommandBuffer> m_cmdbuf[FRAMES_IN_FLIGHT] = { nullptr };
+
+	nbl::video::ISwapchain::SCreationParams m_swapchainCreationParams;
+
+
+public:
+	void setWindow(core::smart_refctd_ptr<nbl::ui::IWindow>&& wnd) override
+	{
+		window = std::move(wnd);
+	}
+	nbl::ui::IWindow* getWindow() override
+	{
+		return window.get();
+	}
+	void setSystem(core::smart_refctd_ptr<nbl::system::ISystem>&& system) override
+	{
+		system = std::move(system);
+	}
+	video::IAPIConnection* getAPIConnection() override
+	{
+		return apiConnection.get();
+	}
+	video::ILogicalDevice* getLogicalDevice()  override
+	{
+		return logicalDevice.get();
+	}
+	video::IGPURenderpass* getRenderpass() override
+	{
+		return renderpass.get();
+	}
+	void setSurface(core::smart_refctd_ptr<video::ISurface>&& s) override
+	{
+		surface = std::move(s);
+	}
+	void setFBOs(std::vector<core::smart_refctd_ptr<video::IGPUFramebuffer>>& f) override
+	{
+		for (int i = 0; i < f.size(); i++)
+		{
+			auto& fboDynArray = *(framebuffersDynArraySmartPtr.get());
+			fboDynArray[i] = core::smart_refctd_ptr(f[i]);
+		}
+	}
+	void setSwapchain(core::smart_refctd_ptr<video::ISwapchain>&& s) override
+	{
+		swapchain = std::move(s);
+	}
+	uint32_t getSwapchainImageCount() override
+	{
+		return swapchain->getImageCount();
+	}
+	virtual nbl::asset::E_FORMAT getDepthFormat() override
+	{
+		return nbl::asset::EF_D32_SFLOAT;
+	}
+
+	APP_CONSTRUCTOR(CADApp);
+
+	void onAppInitialized_impl() override
+	{
+		const auto swapchainImageUsage = static_cast<asset::IImage::E_USAGE_FLAGS>(asset::IImage::EUF_COLOR_ATTACHMENT_BIT | asset::IImage::EUF_STORAGE_BIT | asset::IImage::EUF_TRANSFER_DST_BIT);
+		std::array<asset::E_FORMAT, 1> acceptableSurfaceFormats = { asset::EF_B8G8R8A8_UNORM };
+
+		CommonAPI::InitParams initParams;
+		initParams.windowCb = core::smart_refctd_ptr<CommonAPI::CommonAPIEventCallback>(this);
+		initParams.window = core::smart_refctd_ptr(window);
+		initParams.apiType = video::EAT_VULKAN;
+		initParams.appName = { "62.CAD" };
+		initParams.framesInFlight = FRAMES_IN_FLIGHT;
+		initParams.windowWidth = WIN_W;
+		initParams.windowHeight = WIN_H;
+		initParams.swapchainImageCount = 3u;
+		initParams.swapchainImageUsage = swapchainImageUsage;
+		initParams.acceptableSurfaceFormats = acceptableSurfaceFormats.data();
+		initParams.acceptableSurfaceFormatCount = acceptableSurfaceFormats.size();
+		auto initOutput = CommonAPI::InitWithDefaultExt(std::move(initParams));
+
+		system = std::move(initOutput.system);
+		window = std::move(initParams.window);
+		windowCb = std::move(initParams.windowCb);
+		apiConnection = std::move(initOutput.apiConnection);
+		surface = std::move(initOutput.surface);
+		physicalDevice = std::move(initOutput.physicalDevice);
+		logicalDevice = std::move(initOutput.logicalDevice);
+		utilities = std::move(initOutput.utilities);
+		queues = std::move(initOutput.queues);
+		assetManager = std::move(initOutput.assetManager);
+		cpu2gpuParams = std::move(initOutput.cpu2gpuParams);
+		logger = std::move(initOutput.logger);
+		inputSystem = std::move(initOutput.inputSystem);
+		windowManager = std::move(initOutput.windowManager);
+		m_swapchainCreationParams = std::move(initOutput.swapchainCreationParams);
+
+		CommonAPI::createSwapchain(std::move(logicalDevice), m_swapchainCreationParams, initParams.windowWidth, initParams.windowHeight, swapchain);
+
+		commandPools = std::move(initOutput.commandPools);
+		const auto& computeCommandPools = commandPools[CommonAPI::InitOutput::EQT_COMPUTE];
+
+		const uint32_t swapchainImageCount = swapchain->getImageCount();
+		m_swapchainImages = core::make_refctd_dynamic_array<core::smart_refctd_dynamic_array<core::smart_refctd_ptr<video::IGPUImage>>>(swapchainImageCount);
+		framebuffersDynArraySmartPtr = CommonAPI::createFBOWithSwapchainImages(
+			swapchain->getImageCount(), WIN_W, WIN_H,
+			logicalDevice, swapchain, renderpass,
+			getDepthFormat()
+		);
+		video::IGPUObjectFromAssetConverter CPU2GPU;
+
+		//const char* pathToShader = "../compute.hlsl";
+		//core::smart_refctd_ptr<video::IGPUSpecializedShader> specializedShader = nullptr;
+		//{
+		//	asset::IAssetLoader::SAssetLoadParams params = {};
+		//	params.logger = logger.get();
+		//	auto spec = (assetManager->getAsset(pathToShader, params).getContents());
+		//	auto specShader_cpu = core::smart_refctd_ptr_static_cast<asset::ICPUSpecializedShader>(*assetManager->getAsset(pathToShader, params).getContents().begin());
+		//	specializedShader = CPU2GPU.getGPUObjectsFromAssets(&specShader_cpu, &specShader_cpu + 1, cpu2gpuParams)->front();
+		//}
+		//assert(specializedShader);
+		
+		// TODO:
+		// Write Vertex and Fragment Shader
+		// Load Vertex and Fragment Shader
+		// Create DescriptorSetLayout
+		// Create DescriptorSets
+		// Create PipelineLayout from DescriptorSetLayout
+		// Create Pipeline with correct params
+		// Create OrthoCamera
+		
+		// Create Vertex/Index Buffer through a CADDrawData or something
+		// add polyline to the helper class
+
+		for (size_t i = 0; i < FRAMES_IN_FLIGHT; i++)
+		{
+			logicalDevice->createCommandBuffers(
+				computeCommandPools[i].get(),
+				video::IGPUCommandBuffer::EL_PRIMARY,
+				1,
+				m_cmdbuf + i);
+		}
+
+		for (uint32_t i = 0u; i < FRAMES_IN_FLIGHT; i++)
+		{
+			m_imageAcquire[i] = logicalDevice->createSemaphore();
+			m_renderFinished[i] = logicalDevice->createSemaphore();
+		}
+	}
+
+	void onAppTerminated_impl() override
+	{
+		logicalDevice->waitIdle();
+	}
+
+	void workLoopBody() override
+	{
+		m_resourceIx++;
+		if (m_resourceIx >= FRAMES_IN_FLIGHT)
+			m_resourceIx = 0;
+
+		auto& cb = m_cmdbuf[m_resourceIx];
+		auto& commandPool = commandPools[CommonAPI::InitOutput::EQT_COMPUTE][m_resourceIx];
+		auto& fence = m_frameComplete[m_resourceIx];
+
+		// TODO:
+		// Input and change zoom based on camera
+		// Update UniformBuffer/ConstantBuffer data with camera shit.
+
+		logicalDevice->blockForFences(1u, &fence.get());
+		logicalDevice->resetFences(1u, &fence.get());
+
+		uint32_t imgnum = 0u;
+		auto acquireResult = swapchain->acquireNextImage(m_imageAcquire[m_resourceIx].get(), nullptr, &imgnum);
+		assert(acquireResult == video::ISwapchain::E_ACQUIRE_IMAGE_RESULT::EAIR_SUCCESS);
+
+		uint32_t windowWidth = swapchain->getCreationParameters().width;
+		uint32_t windowHeight = swapchain->getCreationParameters().height;
+
+		// safe to proceed
+		cb->reset(video::IGPUCommandBuffer::ERF_RELEASE_RESOURCES_BIT); // TODO: Begin doesn't release the resources in the command pool, meaning the old swapchains never get dropped
+		cb->begin(video::IGPUCommandBuffer::EU_ONE_TIME_SUBMIT_BIT); // TODO: Reset Frame's CommandPool
+
+		asset::SViewport vp;
+		vp.minDepth = 1.f;
+		vp.maxDepth = 0.f;
+		vp.x = 0u;
+		vp.y = 0u;
+		vp.width = windowWidth;
+		vp.height = windowHeight;
+		cb->setViewport(0u, 1u, &vp);
+
+		VkRect2D scissor;
+		scissor.extent = { windowWidth, windowHeight };
+		scissor.offset = { 0, 0 };
+		cb->setScissor(0u, 1u, &scissor);
+
+		// TODO
+		// Bind DescriptorSet, GraphicsPipeline
+		// BindVertexBuffer, BindIndexBuffer
+		// Issue cb->drawIndexed();
+
+		cb->end();
+
+		CommonAPI::Submit(
+			logicalDevice.get(),
+			cb.get(),
+			queues[CommonAPI::InitOutput::EQT_COMPUTE],
+			m_imageAcquire[m_resourceIx].get(),
+			m_renderFinished[m_resourceIx].get(),
+			fence.get());
+
+
+		CommonAPI::Present(
+			logicalDevice.get(),
+			swapchain.get(),
+			queues[CommonAPI::InitOutput::EQT_COMPUTE],
+			m_renderFinished[m_resourceIx].get(),
+			imgnum);
+	}
+
+	bool keepRunning() override
+	{
+		return windowCb->isWindowOpen();
+	}
+};
+
+//NBL_COMMON_API_MAIN(CADApp)
+int main(int argc, char** argv) {
+	CommonAPI::main<CADApp>(argc, argv);
+}
