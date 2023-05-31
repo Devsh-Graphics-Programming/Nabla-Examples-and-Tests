5104429df60a5d148e80eb78510878badfa2f029
devsh <devsh.graphicsprogramming@gmail.com>
2023-05-31 23:09:17 +0200

    fix example 05 and make the syntax in ex 2 better

--- 02.ComputeShader/main.cpp
+++ 02.ComputeShader/main.cpp
@@ -213,10 +213,7 @@ public:
 		}
 		auto cpuImage = asset::IAsset::castDown<asset::ICPUImageView>(*cpuImageContents.begin());
 		// fix up usage flags to not get validation errors (TODO: Remove when Asset Converter 2.0 comes)
-		{
-			auto& usage = const_cast<core::bitflag<asset::IImage::E_USAGE_FLAGS>&>(cpuImage->getCreationParameters().image->getCreationParameters().usage);
-			usage |= asset::IImage::EUF_STORAGE_BIT; // to write dst mips
-		}
+		cpuImage->getCreationParameters().image->addImageUsageFlags(asset::IImage::EUF_STORAGE_BIT);
 
 		cpu2gpuParams.beginCommandBuffers();
 		auto inImage = CPU2GPU.getGPUObjectsFromAssets(&cpuImage, &cpuImage+1, cpu2gpuParams);
