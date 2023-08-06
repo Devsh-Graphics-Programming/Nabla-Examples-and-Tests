682a8c1e685afea9d72c2984ddea615e12d250ce
atkurtul <atilkurtulmus@gmail.com>
2023-08-06 23:38:12 +0300

    update to latest changes

--- 63.CUDAInterop/main.cpp
+++ 63.CUDAInterop/main.cpp
@@ -39,8 +39,8 @@ if (auto re = expr; NVRTC_SUCCESS != re) { \
 	abort(); \
 }
 
-constexpr uint32_t gridDim[3] = { 256,1,1 };
-constexpr uint32_t blockDim[3] = { 256,1,1 };
+constexpr uint32_t gridDim[3] = { 4096,1,1 };
+constexpr uint32_t blockDim[3] = { 1024,1,1 };
 size_t numElements = gridDim[0] * blockDim[0];
 size_t size = sizeof(float) * numElements;
 
@@ -55,8 +55,9 @@ struct CUDA2VK
 	std::array<core::smart_refctd_ptr<asset::ICPUBuffer>, 2> cpubuffers;
 	std::array<core::smart_refctd_ptr<video::CCUDASharedMemory>, 3> mem = {};
 	core::smart_refctd_ptr<video::CCUDASharedSemaphore> cusema;
-	core::smart_refctd_ptr<video::IGPUBuffer> importedbuf, stagingbuf;
-	core::smart_refctd_ptr<video::IGPUImage> importedimg, stagingimg;
+
+	core::smart_refctd_ptr<video::IGPUBuffer> importedbuf, stagingbuf, stagingbuf2;
+	core::smart_refctd_ptr<video::IGPUImage> importedimg;
 	core::smart_refctd_ptr<video::IGPUSemaphore> sema;
 	core::smart_refctd_ptr<video::IGPUCommandPool> commandPool;
 	core::smart_refctd_ptr<video::IGPUCommandBuffer> cmd;
@@ -84,39 +85,46 @@ struct CUDA2VK
 		for (auto& buf : cpubuffers)
 			buf = core::make_smart_refctd_ptr<asset::ICPUBuffer>(size);
 
-		//for (auto j = 0; j < 2; j++)
-		//	for (auto i = 0; i < numElements; i++)
-		//		reinterpret_cast<float*>(cpubuffers[j]->getPointer())[i] = rand() / float(RAND_MAX);
-
-		memset(cpubuffers[1]->getPointer(), 0, size);
-		uint16_t (*ptr)[2] = reinterpret_cast<uint16_t(*)[2]>(cpubuffers[0]->getPointer());
-		for (auto i = 0; i < gridDim[0]; i++)
-			for (auto j = 0; j < blockDim[0]; j++)
-			{
-				ptr[i * blockDim[0] + j][0] = i;
-				ptr[i * blockDim[0] + j][1] = j;
-			}
+		for (auto j = 0; j < 2; j++)
+			for (auto i = 0; i < numElements; i++)
+				reinterpret_cast<float*>(cpubuffers[j]->getPointer())[i] = rand() / float(RAND_MAX);
 
 		sema = logicalDevice->createSemaphore({ .externalHandleTypes = video::IGPUSemaphore::EHT_OPAQUE_WIN32 });
 		ASSERT_SUCCESS(cudaDevice->importGPUSemaphore(&cusema, sema.get()));
 
-		ASSERT_SUCCESS(cudaDevice->createSharedMemory(&mem[0], { size, sizeof(float), CU_MEM_LOCATION_TYPE_DEVICE }));
-		ASSERT_SUCCESS(cudaDevice->createSharedMemory(&mem[1], { size, sizeof(float), CU_MEM_LOCATION_TYPE_DEVICE }));
-		ASSERT_SUCCESS(cudaDevice->createSharedMemory(&mem[2], { size, sizeof(float), CU_MEM_LOCATION_TYPE_DEVICE }));
+		ASSERT_SUCCESS(cudaDevice->createSharedMemory(&mem[0], { .size = size, .alignment = sizeof(float), .location = CU_MEM_LOCATION_TYPE_DEVICE }));
+		ASSERT_SUCCESS(cudaDevice->createSharedMemory(&mem[1], { .size = size, .alignment = sizeof(float), .location = CU_MEM_LOCATION_TYPE_DEVICE }));
+		ASSERT_SUCCESS(cudaDevice->createSharedMemory(&mem[2], { .size = size, .alignment = sizeof(float), .location = CU_MEM_LOCATION_TYPE_DEVICE }));
+		
+		{
+			auto devmemory = mem[2]->exportAsMemory(logicalDevice);
+			assert(devmemory);
+			video::IGPUBuffer::SCreationParams params = {};
+			params.size = devmemory->getAllocationSize();
+			params.usage = asset::IBuffer::EUF_STORAGE_BUFFER_BIT | asset::IBuffer::EUF_TRANSFER_SRC_BIT;
+			params.externalHandleTypes = video::CCUDADevice::EXTERNAL_MEMORY_HANDLE_TYPE;
+			importedbuf = logicalDevice->createBuffer(std::move(params));
+			assert(importedbuf);
+			bool re = logicalDevice->bindBufferMemory(video::ILogicalDevice::SBindBufferMemoryInfo{.buffer = importedbuf.get(), .memory = devmemory.get() });
+			assert(re);
+		}
 
-		importedbuf = mem[2]->exportAsBuffer(logicalDevice, asset::IBuffer::EUF_STORAGE_BUFFER_BIT | asset::IBuffer::EUF_TRANSFER_SRC_BIT);
-		importedimg = mem[2]->exportAsImage(logicalDevice,
-			asset::IImage::SCreationParams{
-				.type = asset::IImage::ET_2D,
-				.samples = asset::IImage::ESCF_1_BIT,
-				.format  = asset::EF_R32_SFLOAT,
-				.extent = { gridDim[0], blockDim[0], 1 },
-				.mipLevels = 1,
-				.arrayLayers = 1,
-				.usage = asset::IImage::EUF_STORAGE_BIT | asset::IImage::EUF_TRANSFER_SRC_BIT,
-			});
-
-		assert(importedbuf);
+		{
+			
+			video::IGPUImage::SCreationParams params = {};
+			params.type = video::IGPUImage::ET_2D;
+			params.samples = video::IGPUImage::ESCF_1_BIT;
+			params.format = asset::EF_R32_SFLOAT;
+			params.extent = { gridDim[0], blockDim[0], 1 };
+			params.mipLevels = 1;
+			params.arrayLayers = 1;
+			params.usage = video::IGPUImage::EUF_STORAGE_BIT | video::IGPUImage::EUF_TRANSFER_SRC_BIT;
+			params.externalHandleTypes = video::CCUDADevice::EXTERNAL_MEMORY_HANDLE_TYPE;
+			params.tiling = video::IGPUImage::ET_LINEAR;
+			importedimg = mem[2]->exportAsImage(logicalDevice, std::move(params));
+			assert(importedimg);
+		}
+		
 		fence = logicalDevice->createFence(video::IGPUFence::ECF_UNSIGNALED);
 		commandPool = logicalDevice->createCommandPool(queues[CommonAPI::InitOutput::EQT_COMPUTE]->getFamilyIndex(), {});
 		bool re = logicalDevice->createCommandBuffers(commandPool.get(), video::IGPUCommandBuffer::EL_PRIMARY, 1, &cmd);
@@ -136,34 +144,8 @@ struct CUDA2VK
 			return buf;
 		};
 
-		auto createStagingImg = [logicalDevice = logicalDevice]()
-		{
-			video::IGPUImage::SCreationParams params = {{
-				.type = asset::IImage::ET_2D,
-				.samples = asset::IImage::ESCF_1_BIT,
-				.format = asset::EF_R32_SFLOAT,
-				.extent = { gridDim[0], blockDim[0], 1 },
-				.mipLevels = 1,
-				.arrayLayers = 1,
-				.usage = asset::IImage::EUF_TRANSFER_DST_BIT 
-			}};
-			params.tiling = video::IGPUImage::ET_LINEAR;
-
-			auto img = logicalDevice->createImage(std::move(params));
-			auto req = img->getMemoryReqs();
-			req.memoryTypeBits &= logicalDevice->getPhysicalDevice()->getDownStreamingMemoryTypeBits();
-			auto allocation = logicalDevice->allocate(req, img.get());
-			assert(allocation.memory && allocation.offset != video::ILogicalDevice::InvalidMemoryOffset);
-			assert(img->getBoundMemory()->isMappable());
-			size_t sz = img->getImageDataSizeInBytes();
-			logicalDevice->mapMemory(video::IDeviceMemoryAllocation::MappedMemoryRange(img->getBoundMemory(), img->getBoundMemoryOffset(), req.size), video::IDeviceMemoryAllocation::EMCAF_READ);
-			assert(img->getBoundMemory()->getMappedPointer());
-			memset(img->getBoundMemory()->getMappedPointer(), 0, req.size);
-			return img;
-		};
-
 		stagingbuf = createStaging();
-		stagingimg = createStagingImg();
+		stagingbuf2 = createStaging();
 	}
 
 	void launchKernel(CUfunction kernel, CUstream stream)
@@ -173,7 +155,11 @@ struct CUDA2VK
 		auto& cu = cudaHandler->getCUDAFunctionTable();
 		// Launch kernel
 		{
-			CUdeviceptr ptrs[] = { mem[0]->getDevicePtr(), mem[1]->getDevicePtr(), mem[2]->getDevicePtr() };
+			CUdeviceptr ptrs[] = {
+				mem[0]->getDeviceptr(),
+				mem[1]->getDeviceptr(),
+				mem[2]->getDeviceptr(),
+			};
 			void* parameters[] = { &ptrs[0], &ptrs[1], &ptrs[2], &numElements };
 			ASSERT_SUCCESS(cu.pcuMemcpyHtoDAsync_v2(ptrs[0], cpubuffers[0]->getPointer(), size, stream));
 			ASSERT_SUCCESS(cu.pcuMemcpyHtoDAsync_v2(ptrs[1], cpubuffers[1]->getPointer(), size, stream));
@@ -182,7 +168,7 @@ struct CUDA2VK
 			auto semaphore = cusema->getInternalObject();
 			ASSERT_SUCCESS(cu.pcuSignalExternalSemaphoresAsync(&semaphore, &signalParams, 1, stream)); // Signal the imported semaphore
 		}
-
+		
 		// After the cuda kernel has signalled our exported vk semaphore, we will download the results through the buffer imported from CUDA
 		{
 			video::IGPUSemaphore* waitSemaphores[] = { sema.get() };
@@ -195,11 +181,19 @@ struct CUDA2VK
 				.dstQueueFamilyIndex = queue->getFamilyIndex(),
 				.buffer = importedbuf,
 				.offset = 0,
-				.size = VK_WHOLE_SIZE,
+				.size = size,
 			};
 
-			video::IGPUCommandBuffer::SImageMemoryBarrier imgBarriers[2] = {{
+			bool re = true;
+			re &= cmd->begin(video::IGPUCommandBuffer::EU_ONE_TIME_SUBMIT_BIT);
+			/*Acquire?*/
+			re &= cmd->pipelineBarrier(asset::EPSF_ALL_COMMANDS_BIT, asset::EPSF_ALL_COMMANDS_BIT, asset::EDF_NONE, 0u, nullptr, 1u, &bufBarrier, 0u, nullptr); 	// Ownership transfer?
+			asset::SBufferCopy region = { .size = size };
+			re &= cmd->copyBuffer(importedbuf.get(), stagingbuf.get(), 1, &region);
+
+			video::IGPUCommandBuffer::SImageMemoryBarrier imgBarrier = {
 				.barrier = { .dstAccessMask = asset::E_ACCESS_FLAGS::EAF_ALL_ACCESSES_BIT_DEVSH },
+				.oldLayout = asset::IImage::EL_PREINITIALIZED,
 				.newLayout = asset::IImage::EL_TRANSFER_SRC_OPTIMAL,
 				.srcQueueFamilyIndex = VK_QUEUE_FAMILY_EXTERNAL_KHR,
 				.dstQueueFamilyIndex = queue->getFamilyIndex(),
@@ -208,35 +202,20 @@ struct CUDA2VK
 					.aspectMask = asset::IImage::EAF_COLOR_BIT,
 					.levelCount = 1u,
 					.layerCount = 1u,
-				}}, {
-				.barrier = {.dstAccessMask = asset::E_ACCESS_FLAGS::EAF_ALL_ACCESSES_BIT_DEVSH },
-				.newLayout = asset::IImage::EL_TRANSFER_DST_OPTIMAL,
-				.image = stagingimg,
-				.subresourceRange = {
-					.aspectMask = asset::IImage::EAF_COLOR_BIT,
-					.levelCount = 1u,
-					.layerCount = 1u,
-				}}
+				}
 			};
 
-			bool re = true;
-			re &= cmd->begin(video::IGPUCommandBuffer::EU_ONE_TIME_SUBMIT_BIT);
-			/*Acquire?*/
-			re &= cmd->pipelineBarrier(asset::EPSF_ALL_COMMANDS_BIT, asset::EPSF_ALL_COMMANDS_BIT, asset::EDF_NONE, 0u, nullptr, 1u, &bufBarrier, 2u, imgBarriers); 	// Ownership transfer?
-			asset::SBufferCopy region = { .size = importedbuf->getSize() };
-			re &= cmd->copyBuffer(importedbuf.get(), stagingbuf.get(), 1, &region);
-			asset::IImage::SImageCopy imgRegion = { 
-				.srcSubresource = {
-					.aspectMask = imgBarriers[0].subresourceRange.aspectMask,
-					.layerCount = imgBarriers[0].subresourceRange.layerCount,
+			re &= cmd->pipelineBarrier(asset::EPSF_ALL_COMMANDS_BIT, asset::EPSF_ALL_COMMANDS_BIT, asset::EDF_NONE, 0u, nullptr, 0u, nullptr, 1u, &imgBarrier); 
+
+			asset::IImage::SBufferCopy imgRegion = {
+				.imageSubresource = {
+					.aspectMask = imgBarrier.subresourceRange.aspectMask,
+					.layerCount = imgBarrier.subresourceRange.layerCount,
 				},
-				.dstSubresource = {
-					.aspectMask = imgBarriers[1].subresourceRange.aspectMask,
-					.layerCount = imgBarriers[1].subresourceRange.layerCount,
-				},
-				.extent = importedimg->getCreationParameters().extent
+				.imageExtent = importedimg->getCreationParameters().extent,
 			};
-			re &= cmd->copyImage(importedimg.get(), imgBarriers[0].newLayout, stagingimg.get(), imgBarriers[1].newLayout, 1, &imgRegion);
+
+			re &= cmd->copyImageToBuffer(importedimg.get(), imgBarrier.newLayout, stagingbuf2.get(), 1, &imgRegion);
 			re &= cmd->end();
 
 			video::IGPUQueue::SSubmitInfo submitInfo = {
@@ -266,9 +245,9 @@ struct CUDA2VK
 		float* A = reinterpret_cast<float*>(cpubuffers[0]->getPointer());
 		float* B = reinterpret_cast<float*>(cpubuffers[1]->getPointer());
 		float* CBuf = reinterpret_cast<float*>(stagingbuf->getBoundMemory()->getMappedPointer());
-		float* CImg = reinterpret_cast<float*>(stagingimg->getBoundMemory()->getMappedPointer());
+		float* CImg = reinterpret_cast<float*>(stagingbuf2->getBoundMemory()->getMappedPointer());
 
-		assert(!memcmp(CBuf, CImg, size));
+		 assert(!memcmp(CBuf, CImg, size));
 
 		for (auto i = 0; i < numElements; i++)
 		{
@@ -277,8 +256,6 @@ struct CUDA2VK
 		}
 		
 		std::cout << "Success\n";
-
-		delete this;
 	}
 };
 
@@ -297,7 +274,6 @@ int main(int argc, char** argv)
 	auto& utilities = initOutput.utilities;
 	auto& queues = initOutput.queues;
 	auto& logger = initOutput.logger;
-	
 
 	assert(physicalDevice->getLimits().externalMemory);
 	auto cudaHandler = video::CCUDAHandler::create(system.get(), core::smart_refctd_ptr<system::ILogger>(logger));
@@ -322,11 +298,14 @@ int main(int argc, char** argv)
 	ASSERT_SUCCESS(cu.pcuModuleGetFunction(&kernel, module, "vectorAdd"));
 	ASSERT_SUCCESS(cu.pcuStreamCreate(&stream, CU_STREAM_NON_BLOCKING));
 
-	(new CUDA2VK(cudaHandler, cudaDevice, utilities.get(), logicalDevice.get(), queues.data()))->launchKernel(kernel, stream);
-
-	ASSERT_SUCCESS(cu.pcuStreamSynchronize(stream));
+	{
+		auto cuda2vk = CUDA2VK(cudaHandler, cudaDevice, utilities.get(), logicalDevice.get(), queues.data());
+		cuda2vk.launchKernel(kernel, stream);
+		ASSERT_SUCCESS(cu.pcuStreamSynchronize(stream));
+	}
 
 	ASSERT_SUCCESS(cu.pcuModuleUnload(module));
 	ASSERT_SUCCESS(cu.pcuStreamDestroy_v2(stream));
+
 	return 0;
 }
