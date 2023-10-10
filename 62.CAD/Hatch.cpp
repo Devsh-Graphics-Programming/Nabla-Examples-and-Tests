11abbd43adcd2755bd8367cd814dc6bd99bb596d
deprilula28 <deprilula28@gmail.com>
2023-10-10 19:06:41 -0300

    Use HLSL equations

--- 62.CAD/Hatch.cpp
+++ 62.CAD/Hatch.cpp
@@ -1,202 +1,12 @@
 
 #include "Hatch.h"
+using namespace nbl::hlsl;
+
+#include "common.hlsl"
+#include <nbl/builtin/hlsl/equations/hatch.hlsl>
 
 namespace hatchutils {
     
-	// From https://github.com/erich666/GraphicsGems/blob/master/gems/Roots3And4.c
-	// TODO: Refactor this code (needs better names among other things)
-	// These are not fully safe on NaNs or when handling precision, as pointed out by devsh in the Discord
-	static EquationSolveResult<double, 2> SolveQuadratic(double c[3])
-	{
-		double p, q, D;
-
-		/* normal form: x^2 + px + q = 0 */
-
-		p = c[1] / (2 * c[2]);
-		q = c[0] / c[2];
-
-		D = p * p - q;
-
-		if (D == 0.0)
-		{
-			return { 1, { -p } };
-		}
-		else if (D < 0)
-			return { 0, {} };
-		else /* if (D > 0) */
-		{
-			double sqrt_D = sqrt(D);
-			return { 2, { sqrt_D - p, -sqrt_D - p } };
-		}
-	}
-
-	static EquationSolveResult<double, 3> SolveCubic(double c[4])
-	{
-		int     i;
-		double  sub;
-		double  A, B, C;
-		double  sq_A, p, q;
-		double  cb_p, D;
-		EquationSolveResult<double, 3> s;
-
-		/* normal form: x^3 + Ax^2 + Bx + C = 0 */
-
-		A = c[2] / c[3];
-		B = c[1] / c[3];
-		C = c[0] / c[3];
-
-		/*  substitute x = y - A/3 to eliminate quadric term:
-		x^3 +px + q = 0 */
-
-		sq_A = A * A;
-		p = 1.0 / 3 * (-1.0 / 3 * sq_A + B);
-		q = 1.0 / 2 * (2.0 / 27 * A * sq_A - 1.0 / 3 * A * B + C);
-
-		/* use Cardano's formula */
-
-		cb_p = p * p * p;
-		D = q * q + cb_p;
-
-		if (D == 0.0)
-		{
-			if (q == 0.0) /* one triple solution */
-			{
-				s = { 1, { 0 } };
-			}
-			else /* one single and one double solution */
-			{
-				double u = cbrt(-q);
-				s = { 2, { 2 * u, -u } };
-			}
-		}
-		else if (D < 0) /* Casus irreducibilis: three real solutions */
-		{
-			double phi = 1.0 / 3 * acos(-q / sqrt(-cb_p));
-			double t = 2 * sqrt(-p);
-
-			s = { 3, { t * cos(phi), -t * cos(phi + nbl::core::PI<double>() / 3), -t * cos(phi - nbl::core::PI<double>() / 3) } };
-		}
-		else /* one real solution */
-		{
-			double sqrt_D = sqrt(D);
-			double u = cbrt(sqrt_D - q);
-			double v = -cbrt(sqrt_D + q);
-
-			s = { 2, { u + v } };
-		}
-
-		/* resubstitute */
-
-		sub = 1.0 / 3 * A;
-
-		for (i = 0; i < s.uniqueRoots; ++i)
-			s.roots[i] -= sub;
-
-		return s;
-	}
-
-
-	static EquationSolveResult<double, 4> SolveQuartic(double c[5])
-	{
-		double  coeffs[4];
-		double  z, u, v, sub;
-		double  A, B, C, D;
-		double  sq_A, p, q, r;
-		int     i;
-		EquationSolveResult<double, 4> s;
-
-		/* normal form: x^4 + Ax^3 + Bx^2 + Cx + D = 0 */
-
-		A = c[3] / c[4];
-		B = c[2] / c[4];
-		C = c[1] / c[4];
-		D = c[0] / c[4];
-
-		/*  substitute x = y - A/4 to eliminate cubic term:
-		x^4 + px^2 + qx + r = 0 */
-
-		sq_A = A * A;
-		p = -3.0 / 8 * sq_A + B;
-		q = 1.0 / 8 * sq_A * A - 1.0 / 2 * A * B + C;
-		r = -3.0 / 256 * sq_A * sq_A + 1.0 / 16 * sq_A * B - 1.0 / 4 * A * C + D;
-
-		if (r == 0.0)
-		{
-			/* no absolute term: y(y^3 + py + q) = 0 */
-
-			coeffs[0] = q;
-			coeffs[1] = p;
-			coeffs[2] = 0;
-			coeffs[3] = 1;
-
-			auto cubic = SolveCubic(coeffs);
-			s = { cubic.uniqueRoots + 1, {} };
-			std::copy(cubic.roots.begin(), cubic.roots.end(), s.roots.begin());
-			s.roots[cubic.uniqueRoots] = 0;
-		}
-		else
-		{
-			/* solve the resolvent cubic ... */
-
-			coeffs[0] = 1.0 / 2 * r * p - 1.0 / 8 * q * q;
-			coeffs[1] = -r;
-			coeffs[2] = -1.0 / 2 * p;
-			coeffs[3] = 1;
-
-			auto cubic = SolveCubic(coeffs);
-			s = { cubic.uniqueRoots, {} };
-			std::copy(cubic.roots.begin(), cubic.roots.end(), s.roots.begin());
-
-			/* ... and take the one real solution ... */
-
-			z = s.roots[0];
-
-			/* ... to build two quadric equations */
-
-			u = z * z - r;
-			v = 2 * z - p;
-
-			if (u == 0.0)
-				u = 0;
-			else if (u > 0)
-				u = sqrt(u);
-			else
-				return { 0, {} };
-
-			if (v == 0.0)
-				v = 0;
-			else if (v > 0)
-				v = sqrt(v);
-			else
-				return { 0, {} };
-
-			coeffs[0] = z - u;
-			coeffs[1] = q < 0 ? -v : v;
-			coeffs[2] = 1;
-
-			auto quadric1 = SolveQuadratic(coeffs);
-
-			coeffs[0] = z + u;
-			coeffs[1] = q < 0 ? v : -v;
-			coeffs[2] = 1;
-
-			auto quadric2 = SolveQuadratic(coeffs);
-
-			s = { quadric1.uniqueRoots + quadric2.uniqueRoots, {} };
-			std::copy(quadric1.roots.begin(), quadric1.roots.end(), s.roots.begin());
-			std::copy(quadric2.roots.begin(), quadric2.roots.end(), s.roots.begin() + quadric1.uniqueRoots);
-		}
-
-		/* resubstitute */
-
-		sub = 1.0 / 4 * A;
-
-		for (i = 0; i < s.uniqueRoots; ++i)
-			s.roots[i] -= sub;
-
-		return s;
-	}
-
 	// Intended to mitigate issues with NaNs and precision by falling back to using simpler functions when the higher roots are small enough
 	static std::array<double, 4> solveQuarticRoots(double a, double b, double c, double d, double e, double t_start, double t_end)
 	{
@@ -207,20 +17,20 @@ namespace hatchutils {
 		if (std::abs(a) > std::max(std::abs(b), cubCoeffMag) * QUARTIC_THRESHHOLD)
 		{
 			double params[5] = { e, d, c, b, a };
-			auto res = hatchutils::SolveQuartic(params);
-			std::copy(res.roots.data(), res.roots.data() + t.size(), t.begin());
+			auto res = equations::hatch::SolveQuartic(params);
+			memcpy(&t[0], &res.x, sizeof(double) * 4);
 		}
 		else if (abs(b) > quadCoeffMag)
 		{
 			double params[4] = { d, c, b, a };
-			auto res = hatchutils::SolveCubic(params);
-			std::copy(res.roots.data(), res.roots.data() + t.size(), t.begin());
+			auto res = equations::hatch::SolveCubic(params);
+			memcpy(&t[0], &res.x, sizeof(double) * 3);
 		}
 		else
 		{
 			double params[3] = { c, b, a };
-			auto res = hatchutils::SolveQuadratic(params);
-			std::copy(res.roots.data(), res.roots.data() + t.size(), t.begin());
+			auto res = equations::hatch::SolveQuadratic(params);
+			memcpy(&t[0], &res.x, sizeof(double) * 2);
 		}
 
 		if (!(t[0] != t[1]))
