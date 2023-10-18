70b44b8b35280251c82489f9c8851eeb308062c1
Hazard (Cyprian Skrzypczak) <hazard.cspl@gmail.com>
2023-10-18 21:27:12 +0200

    updated test scripts to use nabla package as root

--- 34_LRUCacheUnitTest/test/nbl.py
+++
@@ -1,6 +0,0 @@
-# Copyright (C) 2023 - DevSH Graphics Programming Sp. z O.O.
-
-# Proxy Nabla Python Framework import script
-#from ....tests.src.framework import *
-
-from ....tests.src.framework import *
--- 34_LRUCacheUnitTest/test/test.py
+++ 34_LRUCacheUnitTest/test/test.py
@@ -1,8 +1,8 @@
-from nbl import *
+from .nbl import *
 
 def main():
-    newArgv, config_json_filepaths, nabla_dir, warnings = nbl.Utils.get_args()
-    if nbl.ConsoleOutput.ExpectedFileAsDependencyTest("LRU cache test", config_json_filepaths, nabla_dir, warnings).run():
+    newArgv, config_json_filepaths, nabla_dir, warnings = get_args()
+    if ExpectedFileAsDependencyTest("LRU cache test", config_json_filepaths, nabla_dir, warnings).run():
         print("Test finished, passed")
         exit(0)
     else:
