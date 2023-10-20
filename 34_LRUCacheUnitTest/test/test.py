7645e9a27e9e2b76a34cdf788178dd3340cb8e47
AnastaZIuk <areklachowicz@gmail.com>
2023-10-20 16:16:39 +0200

    update .gitignore and 34_LRUCacheUnitTest/test/test.py interface

--- 34_LRUCacheUnitTest/test/test.py
+++ 34_LRUCacheUnitTest/test/test.py
@@ -1,8 +1,8 @@
-from .nbl import *
+import framework as nbl
 
 def main():
-    newArgv, config_json_filepaths, nabla_dir, warnings = get_args()
-    if ExpectedFileAsDependencyTest("LRU cache test", config_json_filepaths, nabla_dir, warnings).run():
+    newArgv, config_json_filepaths, nabla_dir, warnings = nbl.get_args()
+    if nbl.ExpectedFileAsDependencyTest("LRU cache test", config_json_filepaths, nabla_dir, warnings).run():
         print("Test finished, passed")
         exit(0)
     else:
