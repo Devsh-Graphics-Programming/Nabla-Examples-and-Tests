540647458a281df40b4147938fe9adbaf5ea79fb
Hazard (Cyprian Skrzypczak) <hazard.cspl@gmail.com>
2023-10-27 03:25:15 +0200

    updated test.py and initialized python package

--- 34_LRUCacheUnitTest/test/test.py
+++ 34_LRUCacheUnitTest/test/test.py
@@ -1,7 +1,8 @@
 import framework as nbl
 
-def main():
-    newArgv, config_json_filepaths, nabla_dir, warnings = nbl.get_args()
+def main(args = None, config_json_filepaths = None, nabla_dir = None, warnings = None):
+    if config_json_filepaths is None:
+        args, config_json_filepaths, nabla_dir, warnings = nbl.get_args()
     if nbl.ExpectedFileAsDependencyTest("LRU cache test", config_json_filepaths, nabla_dir, warnings).run():
         print("Test finished, passed")
         exit(0)
