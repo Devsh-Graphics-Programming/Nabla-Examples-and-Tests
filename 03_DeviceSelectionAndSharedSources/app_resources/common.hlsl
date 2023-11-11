e3d7560ce648be044b8deb0482837cb290b1b30c
devsh <devsh.graphicsprogramming@gmail.com>
2023-11-11 14:55:04 +0100

    use the shared C++ and HLSL headers after the fix

--- 03_DeviceSelectionAndSharedSources/app_resources/common.hlsl
+++ 03_DeviceSelectionAndSharedSources/app_resources/common.hlsl
@@ -1,11 +1,4 @@
 // This is a magical header that provides most HLSL types and intrinsics in C++
-//#include "nbl/builtin/hlsl/cpp_compat.hlsl"
-
-#ifdef __HLSL_VERSION
-#define NBL_CONSTEXPR static const
-#else
-#define NBL_CONSTEXPR constexpr
-#endif
-
+#include "nbl/builtin/hlsl/cpp_compat.hlsl"
 
 NBL_CONSTEXPR uint32_t WorkgroupSize = 256;
