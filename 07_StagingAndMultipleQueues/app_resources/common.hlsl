6c880fd9ddebfecf13732425385ad9bccc786aab
devsh <devsh.graphicsprogramming@gmail.com>
2023-11-27 12:43:04 +0100

    get rid of useless files copied over from previous example

--- 07_StagingAndMultipleQueues/app_resources/common.hlsl
+++ 07_StagingAndMultipleQueues/app_resources/common.hlsl
@@ -1,3 +1,5 @@
 #include "nbl/builtin/hlsl/cpp_compat.hlsl"
 
-NBL_CONSTEXPR uint32_t WorkgroupSize = 256;
+NBL_CONSTEXPR uint32_t WorkgroupSizeX = 16;
+NBL_CONSTEXPR uint32_t WorkgroupSizeY = 16;
+NBL_CONSTEXPR uint32_t WorkgroupSize = WorkgroupSizeX*WorkgroupSizeY;
--- 07_StagingAndMultipleQueues/app_resources/shader.comp.hlsl
+++
@@ -1,6 +0,0 @@
-#include "common.hlsl"
-
-[numthreads(WorkgroupSize,1,1)]
-void main(uint32_t3 ID : SV_DispatchThreadID)
-{
-}
