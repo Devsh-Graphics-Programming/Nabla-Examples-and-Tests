bd8877085d1589f96cf6c1ae550b7d1db61d36a1
en_why <yynahim@gmail.com>
2023-12-01 07:26:31 +0100

    create image and copy to buffer

--- 06_HelloGraphicsQueue/main.cpp
+++ 06_HelloGraphicsQueue/main.cpp
@@ -5,7 +5,6 @@
 
 // I've moved out a tiny part of this example into a shared header for reuse, please open and read it.
 #include "../common/MonoDeviceApplication.hpp"
-#include "../common/MonoAssetManagerAndBuiltinResourceApplication.hpp"
 
 using namespace nbl;
 using namespace core;
@@ -15,110 +14,175 @@ using namespace video;
 
 
 // Here we showcase the use of Graphics Queue only 
-class HelloGraphicsQueueApp final : public examples::MonoDeviceApplication, public examples::MonoAssetManagerAndBuiltinResourceApplication
+class HelloGraphicsQueueApp final : public examples::MonoDeviceApplication
 {
-		using device_base_t = examples::MonoDeviceApplication;
-		using asset_base_t = examples::MonoAssetManagerAndBuiltinResourceApplication;
+	using device_base_t = examples::MonoDeviceApplication;
 
-	public:
-		// Yay thanks to multiple inheritance we cannot forward ctors anymore
-		HelloGraphicsQueueApp(const path& _localInputCWD, const path& _localOutputCWD, const path& _sharedInputCWD, const path& _sharedOutputCWD) :
-			system::IApplicationFramework(_localInputCWD,_localOutputCWD,_sharedInputCWD,_sharedOutputCWD) {}
+public:
+	// Yay thanks to multiple inheritance we cannot forward ctors anymore
+	HelloGraphicsQueueApp(const path& _localInputCWD, const path& _localOutputCWD, const path& _sharedInputCWD, const path& _sharedOutputCWD) :
+		system::IApplicationFramework(_localInputCWD, _localOutputCWD, _sharedInputCWD, _sharedOutputCWD) {}
 
-		// This time we will load images and compute their histograms and output them as CSV
-		bool onAppInitialized(smart_refctd_ptr<ISystem>&& system) override
+	// This time we will load images and compute their histograms and output them as CSV
+	bool onAppInitialized(smart_refctd_ptr<ISystem>&& system) override
+	{
+		// Remember to call the base class initialization!
+		if (!device_base_t::onAppInitialized(std::move(system)))
+			return false;
+
+
+		video::IGPUQueue* queue = getGraphicsQueue();
+
+		smart_refctd_ptr<nbl::video::IGPUCommandBuffer> cmdbuf;
 		{
-			// Remember to call the base class initialization!
-			if (!device_base_t::onAppInitialized(std::move(system)))
-				return false;
-			if (!asset_base_t::onAppInitialized(std::move(system)))
-				return false;
-
-			core::smart_refctd_ptr<IGPUCommandBuffer> cmdbuf;
-			if (!m_device->createCommandBuffers(nullptr,IGPUCommandBuffer::EL_PRIMARY,1,&cmdbuf))
-				return false;
-
-			// TODO: for later
-			// - create two EF_R8G8B8A8_SRGB images of 512^2 and 256^2 size
-			// - create a buffer large enough to back the output 256^2 image
-			// - clear one to red the other to blue
-			// - blit the blue in the middle of the red
-			// - blit the red back to blue
-			// - save image to disk
-			// all without using IUtilities
-
-			auto redImage = core::make_smart_refctd_ptr< IGPUImage >;
-			auto blueImage = core::make_smart_refctd_ptr< IGPUImage >;
-
-			IGPUImageView::SCreationParams redParams;
-			redParams.flags = static_cast<IGPUImageView::E_CREATE_FLAGS>(0u);
-			redParams.format = EF_R8G8B8A8_SRGB;
-			redParams.image = core::smart_refctd_ptr<ICPUImage>(redImage);
-			redParams.viewType = IGPUImageView::ET_2D;
-			redParams.subresourceRange = { static_cast<IImage::E_ASPECT_FLAGS>(0u),0u,1u,0u,1u };
-			smart_refctd_ptr<IGPUImageView> redImageView = core::make_smart_refctd_ptr<IGPUImageView>(m_device, redParams);
-
-			IGPUImageView::SCreationParams blueParams;
-			blueParams.flags = static_cast<IGPUImageView::E_CREATE_FLAGS>(0u);
-			blueParams.format = EF_R8G8B8A8_SRGB;
-			blueParams.image = core::smart_refctd_ptr<IGPUImage>(blueImage);
-			blueParams.viewType = IGPUImageView::ET_2D;
-			blueParams.subresourceRange = { static_cast<IImage::E_ASPECT_FLAGS>(0u),0u,1u,0u,1u };
-			smart_refctd_ptr<IGPUImageView> redImageView = core::make_smart_refctd_ptr<IGPUImageView>(m_device, blueParams);
-
-
-			auto buff = core::make_smart_refctd_ptr<nbl::asset::IBuffer>;
-
-			asset::IBuffer::SCreationParams bufferParams;
-			bufferParams.size = 256*256;
-
-
-			cmdbuf->begin();
-			// clear the image to a given colour
-			cmdbuf->clearColorImage();
-			// now blit it into the center of the other one
-			cmdbuf->blitImage(,NEAREST);
-			// blit it again to force downsampling with linear filtering
-			cmdbuf->blitImage(,LINEAR);
-			// copy the resulting image to a buffer
-			cmdbuf->copyImageToBuffer();
-			cmdbuf->end();
-
-			// read the buffer back, create an ICPUImage with an adopted buffer over its contents
-			// save the image to PNG,JPG and whatever other non-EXR format we support writing
-
-			return true;
+			smart_refctd_ptr<nbl::video::IGPUCommandPool> cmdpool = m_device->createCommandPool(queue->getFamilyIndex(), IGPUCommandPool::ECF_TRANSIENT_BIT);
+			if (!m_device->createCommandBuffers(cmdpool.get(), IGPUCommandBuffer::EL_PRIMARY, 1u, &cmdbuf))
+				return logFail("Failed to create Command Buffers!\n");
 		}
 
-		//
-		void workLoopBody() override {}
+		// Imaage Creation parameters
+		nbl::video::IGPUImage::SCreationParams imageParams;
+		imageParams.format = EF_B8G8R8A8_UNORM;
+		imageParams.type = nbl::video::IGPUImage::ET_2D;
+		imageParams.extent.width = 256;
+		imageParams.extent.height = 256;
+		imageParams.extent.depth = 1u;
+		imageParams.mipLevels = 1u;
+		imageParams.arrayLayers = 1u;
+		imageParams.samples = nbl::video::IGPUImage::ESCF_1_BIT;
+		imageParams.flags = nbl::asset::IImage::E_CREATE_FLAGS::ECF_NONE;
+		imageParams.usage = nbl::asset::IImage::EUF_STORAGE_BIT | nbl::asset::IImage::EUF_TRANSFER_DST_BIT | nbl::asset::IImage::EUF_TRANSFER_SRC_BIT;
+		imageParams.initialLayout = nbl::video::IGPUImage::EL_UNDEFINED;
+		imageParams.tiling = nbl::video::IGPUImage::ET_OPTIMAL;
 
-		//
-		bool keepRunning() override {return false;}
+		//Create Image
+		nbl::core::smart_refctd_ptr<nbl::video::IGPUImage> image = m_device->createImage(std::move(imageParams));
+		
+		// Allocate memory to image
+		auto imageMemReqs = image->getMemoryReqs();
+		imageMemReqs.memoryTypeBits &= m_device->getPhysicalDevice()->getDeviceLocalMemoryTypeBits();
+		m_device->allocate(imageMemReqs, image.get());
 
-	protected:
+		// ImageView creation params
+		nbl::video::IGPUImageView::SCreationParams imgViewParams;
+		imgViewParams.flags = static_cast<IGPUImageView::E_CREATE_FLAGS>(0u);
+		imgViewParams.format = image->getCreationParameters().format;
+		imgViewParams.image = image;
+		imgViewParams.viewType = IGPUImageView::ET_2D;
+		imgViewParams.subresourceRange = { static_cast<IImage::E_ASPECT_FLAGS>(IImage::EAF_COLOR_BIT),0u,1u,0u,1u };
 
-		// Override will become irrelevant in the vulkan_1_3 branch
-		SPhysicalDeviceFeatures getRequiredDeviceFeatures() const override
-		{
-			auto retval = device_base_t::getRequiredDeviceFeatures();
-			retval.shaderStorageImageWriteWithoutFormat = true;
-			return retval;
-		}
+		// Create ImageView
+		smart_refctd_ptr<nbl::video::IGPUImageView> imageView = m_device->createImageView(std::move(imgViewParams));
 
-		// Ideally don't want to have to 
-		SPhysicalDeviceFeatures getPreferredDeviceFeatures() const override
-		{
-			auto retval = device_base_t::getPreferredDeviceFeatures();
-			retval.shaderStorageImageReadWithoutFormat = true;
-			return retval;
-		}
+		// Buffer creation params
+		video::IGPUBuffer::SCreationParams bufferParams;
+		bufferParams.size = image->getMemoryReqs().size;
+		bufferParams.usage = asset::IBuffer::EUF_TRANSFER_DST_BIT;
+
+		// Create buffer
+		smart_refctd_ptr<nbl::video::IGPUBuffer> buffer = m_device->createBuffer(std::move(bufferParams));
+
+		// Allocate memory to buffer
+		auto bufferMemReqs = buffer->getMemoryReqs();
+		bufferMemReqs.memoryTypeBits &= m_device->getPhysicalDevice()->getDeviceLocalMemoryTypeBits();
+		m_device->allocate(bufferMemReqs, buffer.get());
+
+		// Clear color values
+		float redColor[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+		SClearColorValue clearColor;
+		memcpy(clearColor.float32, redColor, sizeof(redColor));
 
 
-		core::vector<queue_req_t> getQueueRequirements() const override
-		{
-			return {{.requiredFlags=flags_t::EQF_GRAPHICS_BIT,.disallowedFlags=flags_t::EQF_NONE,.queueCount=1,.maxImageTransferGranularity={1,1,1}}};
-		}
+		asset::IImage::SBufferCopy copyRegion;
+		copyRegion.bufferOffset = 0;
+		copyRegion.bufferRowLength = 0;
+		copyRegion.bufferImageHeight = 0;
+		copyRegion.imageSubresource.aspectMask = asset::IImage::EAF_COLOR_BIT;
+		copyRegion.imageSubresource.mipLevel = 0;
+		copyRegion.imageSubresource.baseArrayLayer = 0;
+		copyRegion.imageSubresource.layerCount = 1;
+		copyRegion.imageOffset = { 0, 0, 0 };
+		copyRegion.imageExtent = { 256, 256, 1 };
+
+		asset::SMemoryBarrier memoryBarrier;
+		memoryBarrier.srcAccessMask = asset::E_ACCESS_FLAGS::EAF_MEMORY_READ_BIT;
+		memoryBarrier.dstAccessMask = asset::E_ACCESS_FLAGS::EAF_MEMORY_WRITE_BIT;
+
+
+		cmdbuf->begin(IGPUCommandBuffer::EU_ONE_TIME_SUBMIT_BIT);
+		// clear the image to a given colour
+		cmdbuf->clearColorImage(image.get(), nbl::video::IGPUImage::EL_GENERAL, &clearColor, 1, &imgViewParams.subresourceRange);
+
+		// Before copyImageToBuffer
+		cmdbuf->pipelineBarrier(
+			EPSF_TRANSFER_BIT, 
+			EPSF_TRANSFER_BIT, 
+			EDF_NONE,
+			1u, &memoryBarrier,
+			0u, nullptr, 
+			0u, nullptr  
+		);
+
+		// copy the resulting image to a buffer
+		cmdbuf->copyImageToBuffer(image.get(), asset::IImage::EL_GENERAL, buffer.get(), 1, &copyRegion);
+		cmdbuf->end();
+
+		auto assetManager = make_smart_refctd_ptr<nbl::asset::IAssetManager>(smart_refctd_ptr(m_system));
+
+
+		ICPUImage::SCreationParams par;
+		par.type = asset::ICPUImage::ET_2D;
+		par.samples = asset::ICPUImage::ESCF_1_BIT;
+		par.format = EF_B8G8R8A8_UNORM;
+		par.extent.width = 256;
+		par.extent.height = 256;
+		par.extent.depth = 1u;
+		par.mipLevels = 1u;
+		par.arrayLayers = 1u;
+		par.flags = nbl::asset::IImage::E_CREATE_FLAGS::ECF_NONE;
+		par.usage = nbl::asset::IImage::EUF_STORAGE_BIT | nbl::asset::IImage::EUF_TRANSFER_DST_BIT | nbl::asset::IImage::EUF_TRANSFER_SRC_BIT;
+
+
+		smart_refctd_ptr<nbl::asset::ICPUImage> cpuImage = asset::ICPUImage::create(par);
+
+		ICPUImageView::SCreationParams imageViewParams;
+		imageViewParams.flags = static_cast<ICPUImageView::E_CREATE_FLAGS>(0u);
+		imageViewParams.format = cpuImage->getCreationParameters().format;;
+		imageViewParams.image = core::smart_refctd_ptr<ICPUImage>(cpuImage);
+		imageViewParams.viewType = ICPUImageView::ET_2D;
+		imageViewParams.subresourceRange = { static_cast<IImage::E_ASPECT_FLAGS>(0u),0u,1u,0u,1u };
+		smart_refctd_ptr<nbl::asset::ICPUImageView> imgView = ICPUImageView::create(std::move(imageViewParams));
+
+
+
+		IAssetWriter::SAssetWriteParams wp(imgView.get());
+		wp.workingDirectory = "";
+		assetManager->writeAsset("jpgWriteSuccessful.png", wp);
+
+
+		// read the buffer back, create an ICPUImage with an adopted buffer over its contents
+		// save the image to PNG,JPG and whatever other non-EXR format we support writing
+
+		return true;
+	}
+
+	//
+	void workLoopBody() override {}
+
+	//
+	bool keepRunning() override { return false; }
+
+private:
+
+	video::IGPUQueue* getGraphicsQueue() const
+	{
+		const auto familyProperties = m_device->getPhysicalDevice()->getQueueFamilyProperties();
+		for (auto i = 0u; i < familyProperties.size(); i++)
+			if (familyProperties[i].queueFlags.hasFlags(video::IPhysicalDevice::E_QUEUE_FLAGS::EQF_GRAPHICS_BIT))
+				return m_device->getQueue(i, 0);
+
+		return nullptr;
+	}
 };
 
 
