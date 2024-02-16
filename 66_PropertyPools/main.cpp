e7b1f9bc5236f457eaa27c2771d875f4564c95f7
deprilula28 <deprilula28@gmail.com>
2024-02-16 15:48:18 -0300

    Remove window app stuff

--- 66_PropertyPools/main.cpp
+++ 66_PropertyPools/main.cpp
@@ -9,151 +9,18 @@
 #include "../common/BasicMultiQueueApplication.hpp"
 #include "../common/MonoAssetManagerAndBuiltinResourceApplication.hpp"
 
-namespace nbl::examples
-{
-
 using namespace nbl;
 using namespace core;
 using namespace system;
 using namespace ui;
 using namespace asset;
 using namespace video;
-
-// Virtual Inheritance because apps might end up doing diamond inheritance
-class WindowedApplication : public virtual BasicMultiQueueApplication
-{
-		using base_t = BasicMultiQueueApplication;
-
-	public:
-		using base_t::base_t;
-
-		virtual video::IAPIConnection::SFeatures getAPIFeaturesToEnable() override
-		{
-			auto retval = base_t::getAPIFeaturesToEnable();
-			// We only support one swapchain mode, surface, the other one is Display which we have not implemented yet.
-			retval.swapchainMode = video::E_SWAPCHAIN_MODE::ESM_SURFACE;
-			return retval;
-		}
-
-		// New function, we neeed to know about surfaces to create ahead of time
-		virtual core::vector<video::SPhysicalDeviceFilter::SurfaceCompatibility> getSurfaces() const = 0;
-
-		virtual core::set<video::IPhysicalDevice*> filterDevices(const core::SRange<video::IPhysicalDevice* const>& physicalDevices) const
-		{
-			const auto firstFilter = base_t::filterDevices(physicalDevices);
-
-			video::SPhysicalDeviceFilter deviceFilter = {};
-			
-			const auto surfaces = getSurfaces();
-			deviceFilter.requiredSurfaceCompatibilities = { surfaces.data(), surfaces.size() };
-
-			return deviceFilter(physicalDevices);
-		}
-		
-		virtual bool onAppInitialized(smart_refctd_ptr<ISystem>&& system)
-		{
-			// Remember to call the base class initialization!
-			if (!base_t::onAppInitialized(std::move(system)))
-				return false;
-
-		#ifdef _NBL_PLATFORM_WINDOWS_
-			m_winMgr = nbl::ui::IWindowManagerWin32::create();
-		#else
-			#error "Unimplemented!"
-		#endif
-		}
-
-		core::smart_refctd_ptr<ui::IWindowManager> m_winMgr;
-};
-
-
-// Before we get onto creating a window, we need to discuss how Nabla handles input, clipboards and cursor control
-class IWindowClosedCallback : public virtual nbl::ui::IWindow::IEventCallback
-{
-	public:
-		IWindowClosedCallback() : m_gotWindowClosedMsg(false) {}
-
-		// unless you create a separate callback per window, both will "trip" this condition
-		bool windowGotClosed() const {return m_gotWindowClosedMsg;}
-
-	private:
-		bool onWindowClosed_impl() override
-		{
-			m_gotWindowClosedMsg = true;
-			return true;
-		}
-
-		bool m_gotWindowClosedMsg;
-};
-
-// We inherit from an application that tries to find Graphics and Compute queues
-// because applications with presentable images often want to perform Graphics family operations
-// Virtual Inheritance because apps might end up doing diamond inheritance
-class SingleNonResizableWindowApplication : public virtual WindowedApplication
-{
-		using base_t = WindowedApplication;
-
-	protected:
-		virtual IWindow::SCreationParams getWindowCreationParams() const
-		{
-			IWindow::SCreationParams params = {};
-			params.callback = make_smart_refctd_ptr<IWindowClosedCallback>();
-			params.width = 640;
-			params.height = 480;
-			params.x = 32;
-			params.y = 32;
-			params.flags = IWindow::ECF_NONE;
-			params.windowCaption = "SingleNonResizableWindowApplication";
-			return params;
-		}
-
-		core::smart_refctd_ptr<ui::IWindow> m_window;
-		core::smart_refctd_ptr<video::ISurfaceVulkan> m_surface;
-
-	public:
-		using base_t::base_t;
-
-		virtual bool onAppInitialized(smart_refctd_ptr<nbl::system::ISystem>&& system) override
-		{
-			// Remember to call the base class initialization!
-			if (!base_t::onAppInitialized(std::move(system)))
-				return false;
-
-			m_window = m_winMgr->createWindow(getWindowCreationParams());
-			m_surface = video::CSurfaceVulkanWin32::create(core::smart_refctd_ptr(m_api),core::smart_refctd_ptr_static_cast<ui::IWindowWin32>(m_window));
-			return true;
-		}
-
-		virtual core::vector<video::SPhysicalDeviceFilter::SurfaceCompatibility> getSurfaces() const
-		{
-			return {{m_surface.get()/*,EQF_NONE*/}};
-		}
-
-		virtual bool keepRunning() override
-		{
-			if (!m_window || reinterpret_cast<const IWindowClosedCallback*>(m_window->getEventCallback())->windowGotClosed())
-				return false;
-
-			return true;
-		}
-};
-}
-
-
-using namespace nbl;
-using namespace core;
-using namespace system;
-using namespace ui;
-using namespace asset;
-using namespace video;
-
 
 #include "app_resources/common.hlsl"
 #include "nbl/builtin/hlsl/bit.hlsl"
 
-
 // In this application we'll cover buffer streaming, Buffer Device Address (BDA) and push constants 
-class PropertyPoolsApp final : public examples::SingleNonResizableWindowApplication, public examples::MonoAssetManagerAndBuiltinResourceApplication
+class PropertyPoolsApp final : public examples::MonoDeviceApplication, public examples::MonoAssetManagerAndBuiltinResourceApplication
 {
 		using device_base_t = examples::MonoDeviceApplication;
 		using asset_base_t = examples::MonoAssetManagerAndBuiltinResourceApplication;
