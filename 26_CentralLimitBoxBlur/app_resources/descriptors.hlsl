543a4d6fc246e197b28d66b6b6b7e0f438037ca0
Eichenherz <transferusuniversalis@gmail.com>
2024-03-06 17:51:14 +0200

    moved accessors to user space

--- 26_CentralLimitBoxBlur/app_resources/descriptors.hlsl
+++ 26_CentralLimitBoxBlur/app_resources/descriptors.hlsl
@@ -1,34 +1,57 @@
 #include "nbl/builtin/hlsl/central_limit_blur/common.hlsl"
+#include "nbl/builtin/hlsl/workgroup/basic.hlsl"
+#include <nbl/builtin/hlsl/glsl_compat/core.hlsl>
 
 [[vk::binding( 0, 0 )]] Texture2D<nbl::hlsl::float32_t4> input;
 [[vk::binding( 1, 0 )]] RWTexture2D<nbl::hlsl::float32_t4> output;
 
-
+template<uint16_t ITEMS_PER_THREAD>
 struct TextureProxy
 {
-	nbl::hlsl::float32_t4 get( const uint32_t2 coordinate )
+	void preload( uint16_t axisIdx, uint16_t itemIdx )
 	{
 		uint32_t3 texSize;
 		input.GetDimensions( 0, texSize.x, texSize.y, texSize.z );
 
 		float32_t4 data = float32_t4(0.f, 0.f, 0.f, 0.f);
+
+		uint32_t2 coordinate = textureCoord( axisIdx, itemIdx );
 		if( all( coordinate < texSize.xy ) )
 		{
 			float32_t4 pixel = input[ coordinate ];
-			data = pixel;// [channel] ;
+			localSpill[ itemIdx ] = pixel;
 		}
-
-		return data;
 	}
 
-	void set( const uint32_t2 coordinate, NBL_CONST_REF_ARG(float32_t4) val )
+	void poststore( uint16_t axisIdx, uint16_t itemIdx )
 	{
 		uint32_t2 texSize;
 		output.GetDimensions( texSize.x, texSize.y );
 
+		uint32_t2 coordinate = textureCoord( axisIdx, itemIdx );
 		if( all( coordinate < texSize ) )
 		{
-			output[ coordinate ] = val;
+			output[ coordinate ] = localSpill[ itemIdx ];
 		}
+	}
+
+	nbl::hlsl::float32_t4 get( uint16_t itemIdx, uint16_t ch )
+	{
+		return localSpill[ itemIdx ][ ch ];
+	}
+
+	void set( uint16_t itemIdx, uint16_t ch, NBL_CONST_REF_ARG( float32_t ) val )
+	{
+		localSpill[ itemIdx ][ ch ] = val;
+	}
+
+//private:
+	float32_t4 localSpill[ ITEMS_PER_THREAD ];
+
+	uint32_t2 textureCoord( uint16_t axisIdx, uint16_t idx )
+	{
+		uint32_t3 coord = nbl::hlsl::glsl::gl_WorkGroupID();
+		coord[ axisIdx ] = ( idx * nbl::hlsl::glsl::gl_WorkGroupSize().x ) + nbl::hlsl::workgroup::SubgroupContiguousIndex();
+		return coord.xy;
 	}
 };
--- 26_CentralLimitBoxBlur/app_resources/main.comp.hlsl
+++ 26_CentralLimitBoxBlur/app_resources/main.comp.hlsl
@@ -12,19 +12,62 @@
 // https://github.com/microsoft/DirectXShaderCompiler/issues/6144
 nbl::hlsl::uint32_t3 nbl::hlsl::glsl::gl_WorkGroupSize() { return uint32_t3( WORKGROUP_SIZE, 1, 1 ); }
 
+static const uint32_t ITEMS_PER_THREAD = 4; // ?
+
+static const uint32_t scratchSz = nbl::hlsl::workgroup::scratch_size_arithmetic<WORKGROUP_SIZE>::value;
+
+groupshared uint32_t scratch[ scratchSz ];
+
+template<typename T, uint16_t offset>
+struct ScratchProxy
+{
+	T get( const uint32_t ix )
+	{
+		return nbl::hlsl::bit_cast< T, uint32_t >( scratch[ ix + offset ] );
+	}
+	void set( const uint32_t ix, NBL_CONST_REF_ARG( T ) value )
+	{
+		scratch[ ix + offset ] = nbl::hlsl::bit_cast< uint32_t, T >( value );
+	}
+
+	void workgroupExecutionAndMemoryBarrier()
+	{
+		nbl::hlsl::glsl::barrier();
+	}
+};
+
 [[vk::push_constant]]
 nbl::hlsl::central_limit_blur::BoxBlurParams boxBlurParams;
 
 [numthreads( WORKGROUP_SIZE, 1, 1 )]
 void main( uint3 invocationID : SV_DispatchThreadID )
 {
-	uint32_t direction = boxBlurParams.getDirection();
-	uint32_t wrapMode = boxBlurParams.getWrapMode();
+	uint16_t axisIdx = 0;// boxBlurParams.getDirection();
+	uint32_t wrapMode = nbl::hlsl::central_limit_blur::WRAP_MODE_CLAMP_TO_EDGE; //boxBlurParams.getWrapMode();
 	nbl::hlsl::float32_t4 borderColor = float32_t4( 1.f, 0.f, 1.f, 1.f );
 	if( boxBlurParams.getWrapMode() == nbl::hlsl::central_limit_blur::WRAP_MODE_CLAMP_TO_BORDER )
 	{
 		borderColor = boxBlurParams.getBorderColor();
 	}
-	TextureProxy textureProxy;
-	nbl::hlsl::central_limit_blur::BoxBlur<4>( boxBlurParams.getChannelCount(), boxBlurParams.radius, wrapMode, borderColor, textureProxy );
+
+	ScratchProxy<float32_t, 0> scratchProxy;
+
+	TextureProxy<ITEMS_PER_THREAD> textureProxy;
+	for( uint16_t i = 0u; i < ITEMS_PER_THREAD; ++i )
+	{
+		textureProxy.preload( axisIdx, i );
+	}
+
+
+	for( uint16_t chIdx = 0u; chIdx < boxBlurParams.getChannelCount(); ++chIdx )
+	{
+		nbl::hlsl::central_limit_blur::BoxBlur<__decltype(textureProxy), __decltype( scratchProxy ), ITEMS_PER_THREAD, scratchSz>( chIdx, boxBlurParams.radius, wrapMode, borderColor, textureProxy, scratchProxy );
+	}
+
+	nbl::hlsl::glsl::barrier();
+	for( uint16_t i = 0; i < ITEMS_PER_THREAD; ++i )
+	{
+		textureProxy.poststore( axisIdx, i );
+	}
+	
 }
