0d3ef46dc61532082c99d13cf2a12bdc1ce79f28
devsh <devsh.graphicsprogramming@gmail.com>
2024-03-14 12:21:50 +0100

    get to the pipeline creation stage

--- 24_ColorSpaceTest/app_resources/present.frag.hlsl
+++ 24_ColorSpaceTest/app_resources/present.frag.hlsl
@@ -1,17 +1,15 @@
 // Copyright (C) 2024-2024 - DevSH Graphics Programming Sp. z O.O.
 // This file is part of the "Nabla Engine".
 // For conditions of distribution and use, see copyright notice in nabla.h
+#pragma wave shader_stage(fragment)
 
 // vertex shader is provided by the fullScreenTriangle extension
-#pragma wave shader_stage(fragment)
+#include <nbl/builtin/hlsl/ext/FullScreenTriangle/SVertexAttributes.hlsl>
 
 [[vk::combinedImageSampler]][[vk::binding(0,3)]] Texture2DArray texture;
 [[vk::combinedImageSampler]][[vk::binding(0,3)]] SamplerState samplerState;
 
-// layout(location = 0) in vec2 TexCoord;
-// layout(location = 0) out vec4 pixelColor;
-
-void main()
+[[vk::location(0)]] float32_t4 main(nbl::hlsl::ext::FullScreenTriangle::SVertexAttributes vxAttr)
 {
-//    pixelColor = textureLod(tex0,TexCoord,0.0);
+    pixelColor = texture.SampleLevel(samplerState,float32_t3(vxAttr.uv,0.f),0.0);
 }
