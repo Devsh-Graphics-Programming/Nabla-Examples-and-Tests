32b40e3c9e8c76f06da979d2bb3344af375bfa6e
Erfan Ahmadi <ahmadierfan99@gmail.com>
2024-04-03 15:00:13 +0330

    [62_CAD] fix resolve_alphas.hlsl to use `nbl::hlsl::jit::device_capabilities`

--- 62_CAD/resolve_alphas.hlsl
+++ 62_CAD/resolve_alphas.hlsl
@@ -2,12 +2,20 @@
 
 #include "common.hlsl"
 #include <nbl/builtin/hlsl/spirv_intrinsics/fragment_shader_pixel_interlock.hlsl>
+#include <nbl/builtin/hlsl/jit/device_capabilities.hlsl>
 
-float4 main(float4 position : SV_Position) : SV_TARGET
+template<bool FragmentShaderPixelInterlock>
+float32_t4 calculateColor(const uint2 fragCoord);
+
+template<>
+float32_t4 calculateColor<false>(const uint2 fragCoord)
 {
-    uint2 fragCoord = uint2(position.xy);
-    
-#if 1
+    return float4(0.0f, 0.0f, 0.0f, 0.0f);
+}
+
+template<>
+float32_t4 calculateColor<true>(const uint2 fragCoord)
+{
     nbl::hlsl::spirv::execution_mode::PixelInterlockOrderedEXT();
     
     nbl::hlsl::spirv::beginInvocationInterlockEXT();
@@ -21,7 +29,9 @@ float4 main(float4 position : SV_Position) : SV_TARGET
     float4 color = lineStyles[mainObjects[mainObjectIdx].styleIdx].color;
     color.a *= float(quantizedAlpha)/255.f;
     return color;
-#else 
-    return float4(0.0f, 0.0f, 0.0f, 0.0f);    
-#endif
+}
+
+float4 main(float4 position : SV_Position) : SV_TARGET
+{
+    return calculateColor<nbl::hlsl::jit::device_capabilities::fragmentShaderPixelInterlock>(position.xy);
 }
