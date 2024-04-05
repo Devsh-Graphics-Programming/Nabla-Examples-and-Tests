05f3429cf4b9b90b35b995855cc6887490080b09
Nipun Garg <nipung271@gmail.com>
2024-04-05 15:16:37 +0530

    Use SubgroupContiguousIndex() for the scatter shader

--- CountingSort/app_resources/scatter_shader.comp.hlsl
+++ CountingSort/app_resources/scatter_shader.comp.hlsl
@@ -5,11 +5,19 @@
 [[vk::push_constant]] PushConstantData pushConstants;
 [[vk::binding(0,0)]] RWStructuredBuffer<uint32_t> scratch;
 
+uint32_t3 nbl::hlsl::glsl::gl_WorkGroupSize()
+{
+    return uint32_t3(WorkgroupSize, 1, 1);
+}
+
 [numthreads(WorkgroupSize, 1, 1)]
 void main(uint32_t3 ID : SV_GroupThreadID, uint32_t3 GroupID : SV_GroupID)
 {
-    uint32_t index = WorkgroupSize * GroupID.x + ID.x;
+    uint32_t tid = nbl::hlsl::workgroup::SubgroupContiguousIndex();
+    uint32_t index = nbl::hlsl::glsl::gl_WorkGroupID().x * WorkgroupSize + tid;
 
+    nbl::hlsl::glsl::barrier();
+    
     if (index < pushConstants.dataElementCount)
     {
         uint32_t value = vk::RawBufferLoad(pushConstants.inputAddress + sizeof(uint32_t) * index);
