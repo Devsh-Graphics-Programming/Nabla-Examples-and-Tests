6c9da55032cbd911471991187af90a8f9a5e92b5
Przemek <minikers21@gmail.com>
2024-04-16 23:48:29 +0200

    Correction in 3rd test shader

--- 03_DeviceSelectionAndSharedSources/app_resources/pplnLayoutMergeTest/shader_3.comp.hlsl
+++ 03_DeviceSelectionAndSharedSources/app_resources/pplnLayoutMergeTest/shader_3.comp.hlsl
@@ -2,7 +2,8 @@
 #include "common.hlsl"
 
 [[vk::binding(0,0)]] RWStructuredBuffer<SomeType> asdf;
-[[vk::binding(1,0)]] RWByteAddressBuffer output[3];
+[[vk::binding(1,0)]] RWStructuredBuffer<SomeType> fdsa;
+[[vk::binding(2,0)]] RWByteAddressBuffer output;
 
 [numthreads(WorkgroupSize, 1, 1)]
 void main(uint32_t3 ID : SV_DispatchThreadID)
@@ -10,5 +11,5 @@ void main(uint32_t3 ID : SV_DispatchThreadID)
     const uint32_t index = ID.x;
 	const uint32_t byteOffset = sizeof(uint32_t)*index;
 
-    output[0].Store<uint32_t>(byteOffset, asdf[index].a);
+    output.Store<uint32_t>(byteOffset, asdf[index].a + fdsa[index].b[2]);
 }
