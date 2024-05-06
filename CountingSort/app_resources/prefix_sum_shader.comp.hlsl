686fcecd706ce30d2341f120c8272c414a3c12dc
Nipun Garg <nipung271@gmail.com>
2024-05-06 23:41:37 +0530

    Move Accessor code to userspace

--- CountingSort/app_resources/prefix_sum_shader.comp.hlsl
+++ CountingSort/app_resources/prefix_sum_shader.comp.hlsl
@@ -1,7 +1,38 @@
 #include "nbl/builtin/hlsl/bda/__ptr.hlsl"
 #include "nbl/builtin/hlsl/sort/counting.hlsl"
+#include "nbl/builtin/hlsl/sort/common.hlsl"
 
 [[vk::push_constant]] nbl::hlsl::sort::CountingPushData pushData;
+
+struct PtrAccessor
+{
+    static PtrAccessor createAccessor(uint64_t addr)
+    {
+        PtrAccessor ptr;
+        ptr.addr = addr;
+        return ptr;
+    }
+
+    uint32_t get(uint64_t index)
+    {
+        return bda::__ptr < uint32_t > (addr + sizeof(uint32_t) * index).template
+        deref().load();
+    }
+
+    void set(uint64_t index, uint32_t value)
+    {
+        bda::__ptr < uint32_t > (addr + sizeof(uint32_t) * index).template
+        deref().store(value);
+    }
+
+    uint32_t atomicAdd(uint64_t index, uint32_t value)
+    {
+        return bda::__ptr < uint32_t > (addr + sizeof(uint32_t) * index).template
+        deref().atomicAdd(value);
+    }
+
+    uint64_t addr;
+};
 
 uint32_t3 nbl::hlsl::glsl::gl_WorkGroupSize()
 {
@@ -11,10 +42,10 @@ uint32_t3 nbl::hlsl::glsl::gl_WorkGroupSize()
 [numthreads(WorkgroupSize,1,1)]
 void main(uint32_t3 ID : SV_GroupThreadID, uint32_t3 GroupID : SV_GroupID)
 {
-    nbl::hlsl::sort::counting < bda::PtrAccessor<uint32_t>, bda::PtrAccessor<uint32_t>, bda::PtrAccessor<uint32_t> > counter;
+    nbl::hlsl::sort::counting <PtrAccessor, PtrAccessor, PtrAccessor> counter;
     counter.histogram(
-        bda::PtrAccessor<uint32_t>::createAccessor(pushData.inputKeyAddress),
-        bda::PtrAccessor<uint32_t>::createAccessor(pushData.scratchAddress),
+        PtrAccessor::createAccessor(pushData.inputKeyAddress),
+        PtrAccessor::createAccessor(pushData.scratchAddress),
         pushData
     );
 }
--- CountingSort/app_resources/scatter_shader.comp.hlsl
+++ CountingSort/app_resources/scatter_shader.comp.hlsl
@@ -1,7 +1,63 @@
 #include "nbl/builtin/hlsl/bda/__ptr.hlsl"
 #include "nbl/builtin/hlsl/sort/counting.hlsl"
+#include "nbl/builtin/hlsl/sort/common.hlsl"
 
 [[vk::push_constant]] nbl::hlsl::sort::CountingPushData pushData;
+
+struct PtrAccessor
+{
+    static PtrAccessor createAccessor(uint64_t addr)
+    {
+        PtrAccessor ptr;
+        ptr.addr = addr;
+        return ptr;
+    }
+
+    uint32_t get(uint64_t index)
+    {
+        return bda::__ptr < uint32_t > (addr + sizeof(uint32_t) * index).template
+        deref().load();
+    }
+
+    void set(uint64_t index, uint32_t value)
+    {
+        bda::__ptr < uint32_t > (addr + sizeof(uint32_t) * index).template
+        deref().store(value);
+    }
+
+    uint32_t atomicAdd(uint64_t index, uint32_t value)
+    {
+        return bda::__ptr < uint32_t > (addr + sizeof(uint32_t) * index).template
+        deref().atomicAdd(value);
+    }
+
+    uint64_t addr;
+};
+
+struct DoublePtrAccessor
+{
+    static DoublePtrAccessor createAccessor(uint64_t in_addr, uint64_t out_addr)
+    {
+        DoublePtrAccessor ptr;
+        ptr.in_addr = in_addr;
+        ptr.out_addr = out_addr;
+        return ptr;
+    }
+
+    uint32_t get(uint64_t index)
+    {
+        return bda::__ptr < uint32_t > (in_addr + sizeof(uint32_t) * index).template
+        deref().load();
+    }
+
+    void set(uint64_t index, uint32_t value)
+    {
+        bda::__ptr < uint32_t > (out_addr + sizeof(uint32_t) * index).template
+        deref().store(value);
+    }
+
+    uint64_t in_addr, out_addr;
+};
 
 uint32_t3 nbl::hlsl::glsl::gl_WorkGroupSize()
 {
@@ -11,13 +67,11 @@ uint32_t3 nbl::hlsl::glsl::gl_WorkGroupSize()
 [numthreads(WorkgroupSize, 1, 1)]
 void main(uint32_t3 ID : SV_GroupThreadID, uint32_t3 GroupID : SV_GroupID)
 {
-    nbl::hlsl::sort::counting < bda::PtrAccessor<uint32_t>, bda::PtrAccessor<uint32_t>, bda::PtrAccessor<uint32_t> > counter;
+    nbl::hlsl::sort::counting <DoublePtrAccessor, DoublePtrAccessor, PtrAccessor> counter;
     counter.scatter(
-        bda::PtrAccessor<uint32_t>::createAccessor(pushData.inputKeyAddress),
-        bda::PtrAccessor<uint32_t>::createAccessor(pushData.inputValueAddress),
-        bda::PtrAccessor<uint32_t>::createAccessor(pushData.scratchAddress),
-        bda::PtrAccessor<uint32_t>::createAccessor(pushData.outputKeyAddress),
-        bda::PtrAccessor<uint32_t>::createAccessor(pushData.outputValueAddress),
+        DoublePtrAccessor::createAccessor(pushData.inputKeyAddress, pushData.outputKeyAddress),
+        DoublePtrAccessor::createAccessor(pushData.inputValueAddress, pushData.outputValueAddress),
+        PtrAccessor::createAccessor(pushData.scratchAddress),
         pushData
     );
 }
