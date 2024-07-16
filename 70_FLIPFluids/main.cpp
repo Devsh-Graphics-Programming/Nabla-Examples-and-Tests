dcaead3cacfc88619f974afce57a6a5f1dedf8d4
keptsecret <sorchon@gmail.com>
2024-07-16 15:06:09 +0700

    init new example

---
+++ 70_FLIPFluids/main.cpp
@@ -0,0 +1,14 @@
+#include "nbl/application_templates/MonoSystemMonoLoggerApplication.hpp"
+
+using namespace nbl;
+using namespace core;
+using namespace system;
+using namespace asset;
+using namespace video;
+
+class FLIPFluidsApp final : public nbl::application_templates::MonoSystemMonoLoggerApplication
+{
+
+};
+
+NBL_MAIN_FUNC(FLIPFluidsApp)
