b6aae0bc401ad1e10a368c716d1fbbc5d64dc58a
AnastaZIuk <areklachowicz@gmail.com>
2024-08-08 17:41:01 +0200

    refactor 61_UI example with Nabla Camera implementation (row major memory layouts with + ImGuizmo mapping column major inputs mapping), leave debug cube & grid in the example

--- 61_UI/main.cpp
+++ 61_UI/main.cpp
@@ -15,6 +15,8 @@
 #include "imgui/imgui_internal.h"
 #include "imguizmo/ImGuizmo.h"
 
+#include "nbl/api/CCamera.hpp"
+
 using namespace nbl;
 using namespace core;
 using namespace hlsl;
@@ -27,7 +29,6 @@ using namespace video;
 // https://github.com/Devsh-Graphics-Programming/ImGuizmo/blob/master/LICENSE
 
 bool useWindow = true;
-int gizmoCount = 1;
 float camDistance = 8.f;
 static ImGuizmo::OPERATION mCurrentGizmoOperation(ImGuizmo::TRANSLATE);
 
@@ -59,114 +60,7 @@ static const float identityMatrix[16] =
 	0.f, 0.f, 1.f, 0.f,
 	0.f, 0.f, 0.f, 1.f };
 
-void Frustum(float left, float right, float bottom, float top, float znear, float zfar, float* m16)
-{
-	float temp, temp2, temp3, temp4;
-	temp = 2.0f * znear;
-	temp2 = right - left;
-	temp3 = top - bottom;
-	temp4 = zfar - znear;
-	m16[0] = temp / temp2;
-	m16[1] = 0.0;
-	m16[2] = 0.0;
-	m16[3] = 0.0;
-	m16[4] = 0.0;
-	m16[5] = temp / temp3;
-	m16[6] = 0.0;
-	m16[7] = 0.0;
-	m16[8] = (right + left) / temp2;
-	m16[9] = (top + bottom) / temp3;
-	m16[10] = (-zfar - znear) / temp4;
-	m16[11] = -1.0f;
-	m16[12] = 0.0;
-	m16[13] = 0.0;
-	m16[14] = (-temp * zfar) / temp4;
-	m16[15] = 0.0;
-}
-
-void Perspective(float fovyInDegrees, float aspectRatio, float znear, float zfar, float* m16)
-{
-	float ymax, xmax;
-	ymax = znear * tanf(fovyInDegrees * 3.141592f / 180.0f);
-	xmax = ymax * aspectRatio;
-	Frustum(-xmax, xmax, -ymax, ymax, znear, zfar, m16);
-}
-
-void Cross(const float* a, const float* b, float* r)
-{
-	r[0] = a[1] * b[2] - a[2] * b[1];
-	r[1] = a[2] * b[0] - a[0] * b[2];
-	r[2] = a[0] * b[1] - a[1] * b[0];
-}
-
-float Dot(const float* a, const float* b)
-{
-	return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
-}
-
-void Normalize(const float* a, float* r)
-{
-	float il = 1.f / (sqrtf(Dot(a, a)) + FLT_EPSILON);
-	r[0] = a[0] * il;
-	r[1] = a[1] * il;
-	r[2] = a[2] * il;
-}
-
-void LookAt(const float* eye, const float* at, const float* up, float* m16)
-{
-	float X[3], Y[3], Z[3], tmp[3];
-
-	tmp[0] = eye[0] - at[0];
-	tmp[1] = eye[1] - at[1];
-	tmp[2] = eye[2] - at[2];
-	Normalize(tmp, Z);
-	Normalize(up, Y);
-
-	Cross(Y, Z, tmp);
-	Normalize(tmp, X);
-
-	Cross(Z, X, tmp);
-	Normalize(tmp, Y);
-
-	m16[0] = X[0];
-	m16[1] = Y[0];
-	m16[2] = Z[0];
-	m16[3] = 0.0f;
-	m16[4] = X[1];
-	m16[5] = Y[1];
-	m16[6] = Z[1];
-	m16[7] = 0.0f;
-	m16[8] = X[2];
-	m16[9] = Y[2];
-	m16[10] = Z[2];
-	m16[11] = 0.0f;
-	m16[12] = -Dot(X, eye);
-	m16[13] = -Dot(Y, eye);
-	m16[14] = -Dot(Z, eye);
-	m16[15] = 1.0f;
-}
-
-void OrthoGraphic(const float l, float r, float b, const float t, float zn, const float zf, float* m16)
-{
-	m16[0] = 2 / (r - l);
-	m16[1] = 0.0f;
-	m16[2] = 0.0f;
-	m16[3] = 0.0f;
-	m16[4] = 0.0f;
-	m16[5] = 2 / (t - b);
-	m16[6] = 0.0f;
-	m16[7] = 0.0f;
-	m16[8] = 0.0f;
-	m16[9] = 0.0f;
-	m16[10] = 1.0f / (zf - zn);
-	m16[11] = 0.0f;
-	m16[12] = (l + r) / (l - r);
-	m16[13] = (t + b) / (b - t);
-	m16[14] = zn / (zn - zf);
-	m16[15] = 1.0f;
-}
-
-void EditTransform(float* cameraView, float* cameraProjection, float* matrix, bool editTransformDecomposition)
+void EditTransform(float* cameraView, const float* cameraProjection, float* matrix, bool editTransformDecomposition)
 {
 	static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::LOCAL);
 	static bool useSnap = false;
@@ -262,7 +156,7 @@ void EditTransform(float* cameraView, float* cameraProjection, float* matrix, bo
 	}
 
 	ImGuizmo::DrawGrid(cameraView, cameraProjection, identityMatrix, 100.f);
-	ImGuizmo::DrawCubes(cameraView, cameraProjection, &objectMatrix[0][0], gizmoCount);
+	ImGuizmo::DrawCubes(cameraView, cameraProjection, &objectMatrix[0][0], 1);
 	ImGuizmo::Manipulate(cameraView, cameraProjection, mCurrentGizmoOperation, mCurrentGizmoMode, matrix, NULL, useSnap ? &snap[0] : NULL, boundSizing ? bounds : NULL, boundSizingSnap ? boundsSnap : NULL);
 
 	ImGuizmo::ViewManipulate(cameraView, camDistance, ImVec2(viewManipulateRight - 128, viewManipulateTop), ImVec2(128, 128), 0x10101010);
@@ -431,15 +325,14 @@ class UISampleApp final : public examples::SimpleWindowedApplication
 					ImGuiIO& io = ImGui::GetIO();
 
 					if (isPerspective)
-					{
-						Perspective(fov, io.DisplaySize.x / io.DisplaySize.y, 0.1f, 100.f, cameraProjection);
-					}
+						camera.setProjectionMatrix(matrix4SIMD::buildProjectionMatrixPerspectiveFovLH(core::radians(fov), io.DisplaySize.x / io.DisplaySize.y, 0.1f, 10000.f));
 					else
 					{
 						float viewHeight = viewWidth * io.DisplaySize.y / io.DisplaySize.x;
-						OrthoGraphic(-viewWidth, viewWidth, -viewHeight, viewHeight, 1000.f, -1000.f, cameraProjection);
+						camera.setProjectionMatrix(matrix4SIMD::buildProjectionMatrixOrthoLH(viewWidth, viewHeight, 0.1f, 10000.f));
 					}
-					ImGuizmo::SetOrthographic(!isPerspective);
+
+					ImGuizmo::SetOrthographic(false);
 					ImGuizmo::BeginFrame();
 
 					ImGui::SetNextWindowPos(ImVec2(1024, 100), ImGuiCond_Appearing);
@@ -449,32 +342,45 @@ class UISampleApp final : public examples::SimpleWindowedApplication
 					ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_Appearing);
 					ImGui::SetNextWindowSize(ImVec2(320, 340), ImGuiCond_Appearing);
 					ImGui::Begin("Editor");
-					if (ImGui::RadioButton("Full view", !useWindow)) useWindow = false;
+
+					if (ImGui::RadioButton("Full view", !useWindow))
+						useWindow = false;
+
 					ImGui::SameLine();
-					if (ImGui::RadioButton("Window", useWindow)) useWindow = true;
+
+					if (ImGui::RadioButton("Window", useWindow))
+						useWindow = true;
 
 					ImGui::Text("Camera");
 					bool viewDirty = false;
-					if (ImGui::RadioButton("Perspective", isPerspective)) isPerspective = true;
+
+					if (ImGui::RadioButton("Perspective", isPerspective))
+						isPerspective = true;
+
 					ImGui::SameLine();
-					if (ImGui::RadioButton("Orthographic", !isPerspective)) isPerspective = false;
+
+					if (ImGui::RadioButton("Orthographic", !isPerspective))
+						isPerspective = false;
+
 					if (isPerspective)
-					{
 						ImGui::SliderFloat("Fov", &fov, 20.f, 110.f);
-					}
 					else
-					{
 						ImGui::SliderFloat("Ortho width", &viewWidth, 1, 20);
-					}
+
 					viewDirty |= ImGui::SliderFloat("Distance", &camDistance, 1.f, 10.f);
-					//ImGui::SliderInt("Gizmo count", &gizmoCount, 1, 4);
 
 					if (viewDirty || firstFrame)
 					{
-						float eye[] = { cosf(camYAngle) * cosf(camXAngle) * camDistance, sinf(camXAngle) * camDistance, sinf(camYAngle) * cosf(camXAngle) * camDistance };
-						float at[] = { 0.f, 0.f, 0.f };
-						float up[] = { 0.f, 1.f, 0.f };
-						LookAt(eye, at, up, cameraView);
+						core::vectorSIMDf cameraPosition(cosf(camYAngle)* cosf(camXAngle)* camDistance, sinf(camXAngle)* camDistance, sinf(camYAngle)* cosf(camXAngle)* camDistance);
+						core::vectorSIMDf cameraTarget(0.f, 0.f, 0.f);
+						const static core::vectorSIMDf up(0.f, 1.f, 0.f);
+
+						camera.setPosition(cameraPosition);
+						camera.setTarget(cameraTarget);
+						camera.setBackupUpVector(up);
+
+						camera.recomputeViewMatrix();
+
 						firstFrame = false;
 					}
 
@@ -494,17 +400,63 @@ class UISampleApp final : public examples::SimpleWindowedApplication
 						ImGui::Text(ImGuizmo::IsOver(ImGuizmo::SCALE) ? "Over scale gizmo" : "");
 					}
 					ImGui::Separator();
-					for (int matId = 0; matId < gizmoCount; matId++)
+					ImGuizmo::SetID(0);
+
+					/*
+					* ImGuizmo expects view & perspective matrix to be column major both with 4x4 layout
+					* and Nabla uses row major matricies - 3x4 matrix for view & 4x4 for projection
+
+					- VIEW:
+
+						ImGuizmo
+
+						|     X[0]          Y[0]          Z[0]         0.0f |
+						|     X[1]          Y[1]          Z[1]         0.0f |
+						|     X[2]          Y[2]          Z[2]         0.0f |
+						| -Dot(X, eye)  -Dot(Y, eye)  -Dot(Z, eye)     1.0f |
+
+						Nabla
+
+						|     X[0]         X[1]           X[2]     -Dot(X, eye)  |
+						|     Y[0]         Y[1]           Y[2]     -Dot(Y, eye)  |
+						|     Z[0]         Z[1]           Z[2]     -Dot(Z, eye)  |
+
+						<ImGuizmo View Matrix> = transpose(nbl::core::matrix4SIMD(<Nabla View Matrix>))
+
+					- PERSPECTIVE [PROJECTION CASE]:
+
+						ImGuizmo
+
+						|      (temp / temp2)                 (0.0)                       (0.0)                   (0.0)  |
+						|          (0.0)                  (temp / temp3)                  (0.0)                   (0.0)  |
+						| ((right + left) / temp2)   ((top + bottom) / temp3)    ((-zfar - znear) / temp4)       (-1.0f) |
+						|          (0.0)                      (0.0)               ((-temp * zfar) / temp4)        (0.0)  |
+
+						Nabla
+
+						|            w                        (0.0)                       (0.0)                   (0.0)               |
+						|          (0.0)                       -h                         (0.0)                   (0.0)               |
+						|          (0.0)                      (0.0)               (-zFar/(zFar-zNear))     (-zNear*zFar/(zFar-zNear)) |
+						|          (0.0)                      (0.0)                      (-1.0)                   (0.0)               |
+
+						<ImGuizmo Projection Matrix> = transpose(<Nabla Projection Matrix>)
+
+					*
+					* the ViewManipulate final call (inside EditTransform) returns world space column major matrix for an object,
+					* note it also modifies input view matrix but projection matrix is immutable
+					*/
+
+					static struct
 					{
-						ImGuizmo::SetID(matId);
+						core::matrix4SIMD view, projection;
+						float* world = nullptr;
+					} imguizmoM16InOut;
 
-						EditTransform(cameraView, cameraProjection, objectMatrix[matId], lastUsing == matId);
-						if (ImGuizmo::IsUsing())
-						{
-							lastUsing = matId;
-						}
-					}
+					imguizmoM16InOut.view = core::transpose(matrix4SIMD(camera.getViewMatrix()));
+					imguizmoM16InOut.projection = core::transpose(camera.getProjectionMatrix());
+					imguizmoM16InOut.world = objectMatrix[0];
 
+					EditTransform(imguizmoM16InOut.view.pointer(), imguizmoM16InOut.projection.pointer(), imguizmoM16InOut.world, true);
 					ImGui::End();
 				}
 			);
@@ -692,19 +644,13 @@ class UISampleApp final : public examples::SimpleWindowedApplication
 		InputSystem::ChannelReader<IMouseEventChannel> mouse;
 		InputSystem::ChannelReader<IKeyboardEventChannel> keyboard;
 
+		Camera camera = Camera(core::vectorSIMDf(0, 0, 0), core::vectorSIMDf(0, 0, 0), core::matrix4SIMD());
+
 		int lastUsing = 0;
-
-		float cameraView[16] =
-		{ 1.f, 0.f, 0.f, 0.f,
-		  0.f, 1.f, 0.f, 0.f,
-		  0.f, 0.f, 1.f, 0.f,
-		  0.f, 0.f, 0.f, 1.f };
-
-		float cameraProjection[16];
 
 		// Camera projection
 		bool isPerspective = true;
-		float fov = 27.f;
+		float fov = 60.f;
 		float viewWidth = 10.f; // for orthographic
 		float camYAngle = 165.f / 180.f * 3.14159f;
 		float camXAngle = 32.f / 180.f * 3.14159f;
