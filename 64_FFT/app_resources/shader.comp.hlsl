442f79fd851c8e29732a8644f0d4163b9c688267
Fletterio <fj.letterio@gmail.com>
2024-08-23 16:59:08 -0300

    Used legacy bda accessor for complex

--- 64_FFT/app_resources/shader.comp.hlsl
+++ 64_FFT/app_resources/shader.comp.hlsl
@@ -1,6 +1,6 @@
 #include "common.hlsl"
 #include "nbl/builtin/hlsl/workgroup/fft.hlsl"
-#include "nbl/builtin/hlsl/bda/bda_accessor.hlsl"
+#include "nbl/builtin/hlsl/bda/legacy_bda_accessor.hlsl"
 
 [[vk::push_constant]] PushConstantData pushConstants;
 
@@ -31,49 +31,14 @@ struct SharedMemoryAccessor
 
 };
 
-// Commented out until we can get BDA working for this
-/* 
-struct Accessor : DoubleBdaAccessor< complex_t<scalar_t> >
+struct Accessor : DoubleLegacyBdaAccessor< complex_t<scalar_t> >
 {
-	static Accessor create(const bda::__ptr< complex_t<scalar_t> > inputPtr, const bda::__ptr< complex_t<scalar_t> > outputPtr)
+	static Accessor create(const uint64_t inputAddress, const uint64_t outputAddress)
     {
         Accessor accessor;
-        accessor.inputPtr = inputPtr;
-        accessor.outputPtr = outputPtr;
+        accessor.inputAddress = inputAddress;
+        accessor.outputAddress = outputAddress;
         return accessor;
-    }
-
-	void memoryBarrier() 
-	{
-		// only one workgroup is touching any memory it wishes to trade
-		spirv::memoryBarrier(spv::ScopeWorkgroup, spv::MemorySemanticsAcquireReleaseMask | spv::MemorySemanticsUniformMemoryMask);
-	}
-
-	void workgroupExecutionAndMemoryBarrier() 
-	{
-		// we're only barriering the workgroup and trading memory within a workgroup
-		spirv::controlBarrier(spv::ScopeWorkgroup, spv::ScopeWorkgroup, spv::MemorySemanticsAcquireReleaseMask | spv::MemorySemanticsUniformMemoryMask);
-    }
-};
-*/
-
-struct Accessor
-{
-	void set(uint32_t idx, complex_t<scalar_t> value) 
-	{
-		vk::RawBufferStore<complex_t<scalar_t> >(pushConstants.outputAddress + sizeof(complex_t<scalar_t>) * idx, value);
-	}
-	
-	void get(uint32_t idx, NBL_REF_ARG(complex_t<scalar_t>) value) 
-	{
-		value = vk::RawBufferLoad<complex_t<scalar_t> >(pushConstants.inputAddress + sizeof(complex_t<scalar_t>) * idx);
-	}
-
-	// Note: Its a funny quirk of the SPIR-V Vulkan Env spec that `MemorySemanticsUniformMemoryMask` means SSBO as well :facepalm: (and probably BDA)
-	void workgroupExecutionAndMemoryBarrier() 
-	{
-		// we're only barriering the workgroup and trading memory within a workgroup
-		spirv::controlBarrier(spv::ScopeWorkgroup, spv::ScopeWorkgroup, spv::MemorySemanticsAcquireReleaseMask | spv::MemorySemanticsUniformMemoryMask);
     }
 
 	void memoryBarrier() 
@@ -86,13 +51,7 @@ struct Accessor
 [numthreads(WorkgroupSize,1,1)]
 void main(uint32_t3 ID : SV_DispatchThreadID)
 {
-	// Commented out until we can get BDA working for this
-	/*
-	bda::__ptr< complex_t<scalar_t> > inputPtr  = bda::__ptr< complex_t<scalar_t> >::create(pushConstants.inputAddress);
-	bda::__ptr< complex_t<scalar_t> > outputPtr = bda::__ptr< complex_t<scalar_t> >::create(pushConstants.outputAddress);
-	Accessor accessor = Accessor::create(inputPtr, outputPtr);
-	*/
-	Accessor accessor;
+	Accessor accessor = Accessor::create(pushConstants.inputAddress, pushConstants.outputAddress);
 	SharedMemoryAccessor sharedmemAccessor;
 
 	// FFT
