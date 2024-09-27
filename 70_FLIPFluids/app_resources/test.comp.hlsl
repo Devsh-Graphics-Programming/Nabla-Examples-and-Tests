b57aca2702f807b99e544ceeffe05ccc805b436b
keptsecret <sorchon@gmail.com>
2024-09-27 11:24:31 +0700

    removed unused shaders + gravity adj

--- 70_FLIPFluids/app_resources/test.comp.hlsl
+++
@@ -1,9 +0,0 @@
-#include "common.hlsl"
-
-[[vk::binding(0,1)]] RWStructuredBuffer<uint32_t> buff;
-
-[numthreads(WorkgroupSize, 1, 1)]
-void main(uint32_t3 ID : SV_DispatchThreadID)
-{
-    buff[ID.x] = ID.x;
-}
--- 70_FLIPFluids/app_resources/test.fragment.hlsl
+++
@@ -1,8 +0,0 @@
-#pragma shader_stage(fragment)
-
-#include "render_common.hlsl"
-
-float4 main(PSInput input) : SV_TARGET
-{
-    return input.color;
-}
