4ab9091804ac39776f29d50672d34e2279813a54
AnastaZIuk <areklachowicz@gmail.com>
2024-10-13 15:45:26 +0200

    nuke CEventCallback from 35_GeometryCreator/main.cpp and use common one instead

--- 35_GeometryCreator/main.cpp
+++ 35_GeometryCreator/main.cpp
@@ -8,6 +8,7 @@
 
 #include "SimpleWindowedApplication.hpp"
 #include "InputSystem.hpp"
+#include "CEventCallback.hpp"
 
 #include "CCamera.hpp"
 #include "SBasicViewParameters.hlsl"
@@ -103,48 +104,6 @@ protected:
 	E_FORMAT m_depthFormat;
 	// only used to pass a parameter from `onCreateSwapchain_impl` to `createFramebuffer`
 	smart_refctd_ptr<IGPUImageView> m_depthBuffer;
-};
-
-class CEventCallback : public ISimpleManagedSurface::ICallback
-{
-public:
-	CEventCallback(nbl::core::smart_refctd_ptr<InputSystem>&& m_inputSystem, nbl::system::logger_opt_smart_ptr&& logger) : m_inputSystem(std::move(m_inputSystem)), m_logger(std::move(logger)) {}
-	CEventCallback() {}
-
-	void setLogger(nbl::system::logger_opt_smart_ptr& logger)
-	{
-		m_logger = logger;
-	}
-	void setInputSystem(nbl::core::smart_refctd_ptr<InputSystem>&& m_inputSystem)
-	{
-		m_inputSystem = std::move(m_inputSystem);
-	}
-private:
-
-	void onMouseConnected_impl(nbl::core::smart_refctd_ptr<nbl::ui::IMouseEventChannel>&& mch) override
-	{
-		m_logger.log("A mouse %p has been connected", nbl::system::ILogger::ELL_INFO, mch.get());
-		m_inputSystem.get()->add(m_inputSystem.get()->m_mouse, std::move(mch));
-	}
-	void onMouseDisconnected_impl(nbl::ui::IMouseEventChannel* mch) override
-	{
-		m_logger.log("A mouse %p has been disconnected", nbl::system::ILogger::ELL_INFO, mch);
-		m_inputSystem.get()->remove(m_inputSystem.get()->m_mouse, mch);
-	}
-	void onKeyboardConnected_impl(nbl::core::smart_refctd_ptr<nbl::ui::IKeyboardEventChannel>&& kbch) override
-	{
-		m_logger.log("A keyboard %p has been connected", nbl::system::ILogger::ELL_INFO, kbch.get());
-		m_inputSystem.get()->add(m_inputSystem.get()->m_keyboard, std::move(kbch));
-	}
-	void onKeyboardDisconnected_impl(nbl::ui::IKeyboardEventChannel* kbch) override
-	{
-		m_logger.log("A keyboard %p has been disconnected", nbl::system::ILogger::ELL_INFO, kbch);
-		m_inputSystem.get()->remove(m_inputSystem.get()->m_keyboard, kbch);
-	}
-
-private:
-	nbl::core::smart_refctd_ptr<InputSystem> m_inputSystem = nullptr;
-	nbl::system::logger_opt_smart_ptr m_logger = nullptr;
 };
 
 class GeometryCreatorApp final : public examples::SimpleWindowedApplication
