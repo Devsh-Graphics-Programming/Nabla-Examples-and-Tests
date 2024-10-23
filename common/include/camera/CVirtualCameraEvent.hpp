ebd0a6e30a634e111c9c600e3fe7aa86dc0781c1
AnastaZIuk <areklachowicz@gmail.com>
2024-10-23 11:53:07 +0200

    update ICameraControl & CVirtualCameraEvent, add processVirtualEvent

--- common/include/camera/CVirtualCameraEvent.hpp
+++ common/include/camera/CVirtualCameraEvent.hpp
@@ -10,52 +10,72 @@ namespace nbl::hlsl
 //! Virtual camera event representing a manipulation
 enum class VirtualEventType 
 {
-    //! Moves the camera forward or backward
-    MoveForward,
-
-    //! Zooms in or out
-    Zoom,
-
-    //! Moves the camera left/right and up/down
+    //! Move the camera in the direction of strafe vector
     Strafe,
 
-    //! Rotates the camera horizontally
-    Pan,
+    //! Update orientation of camera by rotating around X, Y, Z axes
+    Rotate,
 
-    //! Rotates the camera vertically
-    Tilt,
-
-    //! Rolls the camera around the Z axis
-    Roll,
-
-    //! Resets the camera to a default position and orientation
-    Reset
+    //! Signals boolean state, for example "reset"
+    State
 };
-
-//! We encode all manipulation type values into a vec4 & assume they are written in NDC coordinate system with respect to camera
-using manipulation_value_t = float64_t4x4;
 
 class CVirtualCameraEvent 
 {
 public:
-    CVirtualCameraEvent(VirtualEventType type, const manipulation_value_t value)
-        : type(type), value(value) {}
+    using manipulation_encode_t = float32_t4;
 
-    // Returns the type of virtual event
-    VirtualEventType getType() const 
+    struct StrafeManipulation
     {
-        return type;
+        float32_t3 direction = {};
+        float distance = {};
+    };
+
+    struct RotateManipulation
+    {
+        float pitch = {}, roll = {}, yaw = {};
+    };
+
+    struct StateManipulation
+    {
+        uint32_t reset : 1;
+        uint32_t reserved : 31;
+
+        StateManipulation() { memset(this, 0, sizeof(StateManipulation)); }
+        ~StateManipulation() {}
+    };
+
+    union ManipulationValue
+    {
+        StrafeManipulation strafe;
+        RotateManipulation rotation;
+        StateManipulation state;
+
+        ManipulationValue() { memset(this, 0, sizeof(ManipulationValue)); }
+        ~ManipulationValue() {}
+    };
+
+    CVirtualCameraEvent(VirtualEventType type, const ManipulationValue manipulation)
+        : m_type(type), m_manipulation(manipulation)
+    {
+        static_assert(sizeof(manipulation_encode_t) == sizeof(ManipulationValue));
     }
 
-    // Returns the associated value for manipulation
-    manipulation_value_t getValue() const 
+    // Returns the type of manipulation value
+    VirtualEventType getType() const 
     {
-        return value;
+        return m_type;
+    }
+
+    // Returns manipulation value
+    ManipulationValue getManipulation() const
+    {
+        return m_manipulation;
     }
 
 private:
-    VirtualEventType type;
-    manipulation_value_t value;
+    VirtualEventType m_type;
+    ManipulationValue m_manipulation;
 };
 
 }
