80be87d54b6015e0dc1843592824258a7792d87a
AnastaZIuk <areklachowicz@gmail.com>
2024-10-23 11:53:07 +0200

    update ICameraControl & CVirtualCameraEvent, add processVirtualEvent

--- common/include/camera/ICameraControl.hpp
+++ common/include/camera/ICameraControl.hpp
@@ -4,6 +4,7 @@
 #include "IProjection.hpp"
 #include "CVirtualCameraEvent.hpp"
 #include "glm/glm/ext/matrix_transform.hpp" // TODO: TEMPORARY!!! whatever used will be moved to cpp
+#include "glm/glm/gtc/quaternion.hpp"
 
 // TODO: DIFFERENT NAMESPACE
 namespace nbl::hlsl 
@@ -19,30 +20,98 @@ public:
     {
     public:
         CGimbal(const core::smart_refctd_ptr<projection_t>&& projection, const float32_t3& position, const float32_t3& lookat, const float32_t3& upVec = float32_t3(0.0f, 1.0f, 0.0f), const float32_t3& backupUpVec = float32_t3(0.5f, 1.0f, 0.0f))
-            : m_projection(std::move(projection)), m_position(position), m_target(lookat), m_upVec(upVec), m_backupUpVec(backupUpVec), m_initialPosition(position), m_initialTarget(lookat), m_viewMatrix({}), m_isLeftHanded(nbl::hlsl::determinant(m_projection->getProjectionMatrix()) < 0.f)
+            : m_projection(projection), m_position(position), m_target(lookat), m_upVec(upVec), m_backupUpVec(backupUpVec), m_initialPosition(position), m_initialTarget(lookat), m_orientation(glm::quat(1.0f, 0.0f, 0.0f, 0.0f)), m_viewMatrix({}), m_isLeftHanded(isLeftHanded(m_projection->getProjectionMatrix()))
         {
-            //! I make an assumption that we take ownership of projection (just to make less calculations on gimbal update iterations [but maybe I shouldnt, think of it!])
             recomputeViewMatrix();
+        }
+
+        //! Start a gimbal manipulation session
+        inline void begin()
+        {
+            needsToRecomputeViewMatrix = false;
+        }
+
+        //! End the gimbal manipulation session, recompute matrices and check projection
+        inline void end()
+        {
+            m_isLeftHanded = isLeftHanded(m_projection->getProjectionMatrix());
+
+            // Recompute the view matrix
+            if(needsToRecomputeViewMatrix)
+                recomputeViewMatrix();
+
+            needsToRecomputeViewMatrix = false;
+        }
+
+        inline float32_t3 getLocalTarget() const
+        {
+            return m_target - m_position;
+        }
+
+        inline float32_t3 getForwardDirection() const
+        {
+            return glm::normalize(getLocalTarget());
+        }
+
+        //! Reset the gimbal to its initial position, target, and orientation
+        inline void reset()
+        {
+            m_position = m_initialPosition; 
+            m_target = m_initialTarget;
+            m_orientation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+
+            recomputeViewMatrix();  // Recompute the view matrix after resetting
+        }
+
+        //! Move the camera in the direction of strafe (mostly left/right, up/down)
+        void strafe(const glm::vec3& direction, float distance)
+        {
+            if (distance != 0.0f)
+            {
+                const auto strafeVector = glm::normalize(direction) * distance;
+                m_position += strafeVector;
+                m_target += strafeVector;
+
+                needsToRecomputeViewMatrix = true;
+            }
+        }
+
+        //! Update orientation of camera by rotating around all XYZ axes - delta rotations in radians
+        void rotate(float dPitchRadians, float dYawDeltaRadians, float dRollDeltaRadians)
+        {
+            // Rotate around X (pitch)
+            glm::quat qPitch = glm::angleAxis(dPitchRadians, glm::vec3(1.0f, 0.0f, 0.0f));
+
+            // Rotate around Y (yaw)
+            glm::quat qYaw = glm::angleAxis(dYawDeltaRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+
+            // Rotate around Z (roll) // TODO: handness!!
+            glm::quat qRoll = glm::angleAxis(dRollDeltaRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+
+            // Combine the new rotations with the current orientation
+            m_orientation = glm::normalize(qYaw * qPitch * qRoll * m_orientation);
+
+            // Now we have rotation transformation as 3x3 matrix
+            auto rotate = glm::mat3_cast(m_orientation);
+
+            // We do not change magnitude of the vector
+            auto localTargetRotated = rotate * getLocalTarget();
+
+            // And we can simply update target vector
+            m_target = m_position + localTargetRotated;
+
+            // TODO: std::any + nice checks for deltas (radians - periodic!)
+            needsToRecomputeViewMatrix = true;
         }
 
         // TODO: ctor with core::path to json config file to load defaults
 
         const projection_t* getProjection() { return m_projection.get(); }
 
-        // TODO: gimbal methods (to handle VirtualEventType requests)
-
     private:
         inline void recomputeViewMatrix()
         {
-            auto localTarget = glm::normalize(m_target - m_position);
-
-            // If up vector and vector to the target are the same, adjust the up vector
-            auto up = glm::normalize(m_upVec);
-            auto cross = glm::cross(localTarget, up);
-            bool upVectorNeedsChange = glm::dot(cross, cross) == 0;
-
-            if (upVectorNeedsChange)
-                up = glm::normalize(m_backupUpVec);
+            auto up = getPatchedUpVector();
 
             if (m_isLeftHanded)
                 m_viewMatrix = glm::lookAtLH(m_position, m_target, up);
@@ -50,12 +119,37 @@ public:
                 m_viewMatrix = glm::lookAtRH(m_position, m_target, up);
         }
 
+        inline float32_t3 getPatchedUpVector()
+        {
+            // if up vector and vector to the target are the same we patch the up vector
+            auto up = glm::normalize(m_upVec);
+
+            const auto localTarget = getForwardDirection();
+            const auto cross = glm::cross(localTarget, up);
+
+            // we compute squared length but for checking if the len is 0 it doesnt matter 
+            const bool upVectorZeroLength = glm::dot(cross, cross) == 0.f;
+
+            if (upVectorZeroLength)
+                up = glm::normalize(m_backupUpVec);
+
+            return up;
+        }
+
+        inline bool isLeftHanded(const auto& projectionMatrix)
+        {
+            return nbl::hlsl::determinant(projectionMatrix) < 0.f;
+        }
+
         const core::smart_refctd_ptr<projection_t> m_projection;
         float32_t3 m_position, m_target, m_upVec, m_backupUpVec;
         const float32_t3 m_initialPosition, m_initialTarget;
 
+        glm::quat m_orientation;
         float64_t4x4 m_viewMatrix;
-        const bool m_isLeftHanded;
+        bool m_isLeftHanded;
+
+        bool needsToRecomputeViewMatrix = false;
     };
 
     ICameraController(core::smart_refctd_ptr<CGimbal>&& gimbal)
@@ -63,45 +157,22 @@ public:
 
     void processVirtualEvent(const CVirtualCameraEvent& virtualEvent)
     {
-        // we will treat all manipulation event values as NDC, also for non manipulation events
-        // we will define how to handle it, all values are encoded onto vec4 (manipulation_value_t)
-        manipulation_value_t value = virtualEvent.getValue();
+        const auto manipulation = virtualEvent.getManipulation();
 
-        // TODO: this will use gimbal to handle a virtual event registered by a class (wip) which maps physical keys to virtual events
-
-        case VirtualEventType::MoveForward:
-        {
-            // TODO
-        } break;
-    
         case VirtualEventType::Strafe:
         {
-            // TODO
+            m_gimbal->strafe(manipulation.strafe.direction, manipulation.strafe.distance);
+        } break;
+    
+        case VirtualEventType::Rotate:
+        {
+            m_gimbal->rotate(manipulation.rotation.pitch, manipulation.rotation.yaw, manipulation.rotation.roll);
         } break;
 
-        case VirtualEventType::Zoom:
+        case VirtualEventType::State:
         {
-            // TODO
-        } break;
-
-        case VirtualEventType::Pan:
-        {
-            // TODO
-        } break;
-
-        case VirtualEventType::Tilt:
-        {
-            // TODO
-        } break;
-
-        case VirtualEventType::Roll:
-        {
-            // TODO
-        } break;
-
-        case VirtualEventType::Reset:
-        {
-            // TODO
+            if (manipulation.state.reset)
+                m_gimbal->reset();
         } break;
 
         default:
