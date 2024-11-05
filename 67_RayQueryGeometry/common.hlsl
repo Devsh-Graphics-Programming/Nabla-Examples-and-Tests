2bfca3a1f2720373fd9ad5c0dfcb1fd673be3b20
keptsecret <sorchon@gmail.com>
2024-11-05 11:50:32 +0700

    ray query shader

---
+++ 67_RayQueryGeometry/common.hlsl
@@ -0,0 +1,25 @@
+#ifndef RQG_COMMON_HLSL
+#define RQG_COMMON_HLSL
+
+NBL_CONSTEXPR uint32_t WorkgroupSize = 16;
+
+#ifdef __HLSL_VERSION
+struct VertexData
+{
+    float3 position;
+    float4 color;
+    float2 uv;
+    float3 normal;
+};
+
+struct SBasicViewParameters //! matches CPU version size & alignment (160, 4)
+{
+    float3 camPos;
+
+	float4x4 MVP;
+	float3x4 V;
+    float3x4 P;
+};
+#endif
+
+#endif  // RQG_COMMON_HLSL
---
+++ 67_RayQueryGeometry/render.comp.hlsl
@@ -0,0 +1,77 @@
+#include "common.hlsl"
+
+struct SPushConstants
+{
+    uint64_t vertexBufferAddress;
+    uint64_t indexBufferAddress;
+};
+
+[[vk::push_constant]] SPushConstants pc;
+
+[[vk::binding(0, 0)]]
+RaytracingAccelerationStructure topLevelAS;
+
+[[vk::binding(0, 1)]]
+cbuffer CameraData
+{
+    SBasicViewParameters params;
+};
+
+[[vk::binding(0, 2)]] RWTexture2D<float4> outImage;
+
+[numthreads(WorkgroupSize, WorkgroupSize, 1)]
+void main(uint32_t3 threadID : SV_DispatchThreadID)
+{
+    uint2 coords = threadID.xy;
+    uint2 resolution;
+    outImage.GetDimensions(resolution.x, resolution.y);
+    uint2 texCoords = uint2(float(coords.x) / resolution.x, 1.0 - float(coords.y) / resolution.y);
+
+    if (any(coords >= resolution))
+        return;
+
+    float4 color = float4(0, 0, 0, 1);
+
+    const float4x4 invMVP = inverse(cameraData.params.MVP);
+    
+    float4 NDC = float4(texCoords * float2(2.0, -2.0) + float2(-1.0, 1.0), 0.0, 1.0);
+    float3 targetPos;
+    {
+        vec4 tmp = invMVP * NDC;
+        targetPos = tmp.xyz / tmp.w;
+        NDC.z = 1.0;
+    }
+
+    RayDesc ray;
+    ray.TMin = 0.01;
+    ray.TMax = 1000.0;
+    ray.Origin = params.camPos;
+    ray.Direction = normalize(targetPos - params.camPos);
+
+    RayQuery<RAY_FLAG_NONE> query;
+    query.TraceRayInline(topLevelAS, 0, 0xFF, ray);
+
+    while (query.Proceed()) {}
+
+    if (query.CommittedStatus() == COMMITTED_TRIANGLE_HIT)
+    {
+        const int primID = query.CommittedPrimitiveIndex();
+
+        uint idxOffset = primID * 3 * sizeof(uint32_t);
+        uint i0 = vk::RawBufferLoad<uint32_t>(pc.indexBufferAddress + idxOffset);
+        uint i1 = vk::RawBufferLoad<uint32_t>(pc.indexBufferAddress + idxOffset + 1);
+        uint i2 = vk::RawBufferLoad<uint32_t>(pc.indexBufferAddress + idxOffset + 2);
+
+        VertexData v0 = vk::RawBufferLoad<VertexData>(pc.vertexBufferAddress + i0 * sizeof(VertexData));
+        VertexData v1 = vk::RawBufferLoad<VertexData>(pc.vertexBufferAddress + i1 * sizeof(VertexData));
+        VertexData v2 = vk::RawBufferLoad<VertexData>(pc.vertexBufferAddress + i2 * sizeof(VertexData));
+
+        float3 barycentrics = float3(0.0, query.CommittedTriangleBarycentrics());
+        barycentrics.x = 1.0 - barycentrics.y - barycentrics.z;
+
+        float3 normalInterp = barycentrics.x * v0.normal + barycentrics.y * v1.normal + barycentrics.z * v2.normal;
+        color = float4(normalInterp, 1.0);
+    }
+
+    outImage[coords] = color;
+}
