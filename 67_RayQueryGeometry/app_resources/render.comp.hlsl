6a468fb5818e302c76d60a125dbca933ea1c566d
keptsecret <sorchon@gmail.com>
2024-11-14 11:49:35 +0700

    changed how to get index again

--- 67_RayQueryGeometry/app_resources/render.comp.hlsl
+++ 67_RayQueryGeometry/app_resources/render.comp.hlsl
@@ -72,24 +72,35 @@ void main(uint32_t3 threadID : SV_DispatchThreadID)
         const uint byteOffset = 18;
         const uint indexType = 0;   // value from obj.indexType
 
-        uint32_t v0, v1, v2;
+        uint i0, i1, i2;
 
-        if (indexType != 2)
+        switch (indexType)
         {
-            uint i0 = getIndex<indexType>(pc.indexBufferAddress, idxOffset + 0);
-            uint i1 = getIndex<indexType>(pc.indexBufferAddress, idxOffset + 1);
-            uint i2 = getIndex<indexType>(pc.indexBufferAddress, idxOffset + 2);
+            case 0: // EIT_16BIT
+            {
+                i0 = uint32_t(vk::RawBufferLoad<uint16_t>(pc.indexBufferAddress + (idxOffset + 0) * sizeof(uint16_t)));
+                i1 = uint32_t(vk::RawBufferLoad<uint16_t>(pc.indexBufferAddress + (idxOffset + 1) * sizeof(uint16_t)));
+                i2 = uint32_t(vk::RawBufferLoad<uint16_t>(pc.indexBufferAddress + (idxOffset + 2) * sizeof(uint16_t)));
+            }
+            break;
+            case 1: // EIT_32BIT
+            {
+                i0 = vk::RawBufferLoad<uint32_t>(pc.indexBufferAddress + (idxOffset + 0) * sizeof(uint32_t)));
+                i1 = vk::RawBufferLoad<uint32_t>(pc.indexBufferAddress + (idxOffset + 1) * sizeof(uint32_t)));
+                i2 = vk::RawBufferLoad<uint32_t>(pc.indexBufferAddress + (idxOffset + 2) * sizeof(uint32_t)));
+            }
+            break;
+            default:    // EIT_NONE
+            {
+                i0 = idxOffset;
+                i1 = idxOffset + 1;
+                i2 = idxOffset + 2;
+            }
+        }
 
-            v0 = vk::RawBufferLoad<uint32_t>(pc.vertexBufferAddress + i0 * vertexStride + byteOffset);
-            v1 = vk::RawBufferLoad<uint32_t>(pc.vertexBufferAddress + i1 * vertexStride + byteOffset);
-            v2 = vk::RawBufferLoad<uint32_t>(pc.vertexBufferAddress + i2 * vertexStride + byteOffset);
-        }
-        else
-        {
-            v0 = vk::RawBufferLoad<uint32_t>(pc.vertexBufferAddress + (idxOffset + 0) * vertexStride + byteOffset);
-            v1 = vk::RawBufferLoad<uint32_t>(pc.vertexBufferAddress + (idxOffset + 1) * vertexStride + byteOffset);
-            v2 = vk::RawBufferLoad<uint32_t>(pc.vertexBufferAddress + (idxOffset + 2) * vertexStride + byteOffset);
-        }
+        uint32_t v0 = vk::RawBufferLoad<uint32_t>(pc.vertexBufferAddress + i0 * vertexStride + byteOffset);
+        uint32_t v1 = vk::RawBufferLoad<uint32_t>(pc.vertexBufferAddress + i1 * vertexStride + byteOffset);
+        uint32_t v2 = vk::RawBufferLoad<uint32_t>(pc.vertexBufferAddress + i2 * vertexStride + byteOffset);
 
         float3 n0 = getNormalsFromMask(v0) * 0.5 + 0.5;
         float3 n1 = getNormalsFromMask(v1) * 0.5 + 0.5;
@@ -100,6 +111,7 @@ void main(uint32_t3 threadID : SV_DispatchThreadID)
 
         float3 normalInterp = barycentrics.x * n0 + barycentrics.y * n1 + barycentrics.z * n2;
         color = float4(normalInterp, 1.0);
+        //color = float4(0.8, 0, 0.2, 1.0);
     }
 
     outImage[coords] = color;
