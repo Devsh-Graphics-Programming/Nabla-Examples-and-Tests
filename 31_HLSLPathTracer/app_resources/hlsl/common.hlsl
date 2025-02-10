e6a99165c1b153977192f9722381fc24f566c9ca
keptsecret <sorchon@gmail.com>
2025-02-10 16:58:50 +0700

    triangle sampling

--- 31_HLSLPathTracer/app_resources/hlsl/common.hlsl
+++ 31_HLSLPathTracer/app_resources/hlsl/common.hlsl
@@ -4,6 +4,11 @@
 #include <nbl/builtin/hlsl/spirv_intrinsics/core.hlsl>
 #include <nbl/builtin/hlsl/spirv_intrinsics/glsl.std.450.hlsl>
 #include <nbl/builtin/hlsl/numbers.hlsl>
+#include <nbl/builtin/hlsl/shapes/triangle.hlsl>
+#include <nbl/builtin/hlsl/shapes/rectangle.hlsl>
+#include <nbl/builtin/hlsl/sampling/spherical_triangle.hlsl>
+#include <nbl/builtin/hlsl/sampling/projected_spherical_triangle.hlsl>
+//#include <nbl/builtin/hlsl/shapes/rectangle.hlsl>
 
 namespace nbl
 {
@@ -53,6 +58,13 @@ enum ProceduralShapeType : uint16_t
     PST_RECTANGLE
 };
 
+enum PTPolygonMethod : uint16_t
+{
+    PPM_AREA,
+    PPM_SOLID_ANGLE,
+    PPM_APPROX_PROJECTED_SOLID_ANGLE
+};
+
 template<ProceduralShapeType type>
 struct Shape;
 
@@ -95,7 +107,7 @@ struct Shape<PST_SPHERE>
         return 2.0 * numbers::pi<float> * (1.0 - cosThetaMax);
     }
 
-    float deferredPdf(Light light, Ray ray)
+    float deferredPdf(NBL_CONST_REF_ARG(Light light), NBL_CONST_REF_ARG(Ray) ray)
     {
         return 1.0 / getSolidAngle(ray.origin);
     }
@@ -149,6 +161,7 @@ struct Shape<PST_TRIANGLE>
         retval.vertex1 = vertex1;
         retval.vertex2 = vertex2;
         retval.bsdfLightIDs = spirv::bitFieldInsert<uint32_t>(bsdfID, lightID, 16, 16);
+        retval.polygonMethod = PPM_SOLID_ANGLE;
         return retval;
     }
 
@@ -178,12 +191,104 @@ struct Shape<PST_TRIANGLE>
         return nbl::hlsl::cross(edges[0], edges[1]) * 0.5f;
     }
 
+    float deferredPdf(NBL_CONST_REF_ARG(Light light), NBL_CONST_REF_ARG(Ray) ray)
+    {
+        const float32_t3 L = ray.direction;
+        switch (polygonMethod)
+        {
+            case PPM_AREA:
+            {
+                const float dist = ray.intersectionT;
+                return dist * dist / nbl::hlsl::abs(nbl::hlsl::dot(getNormalTimesArea()), L);
+            }
+            break;
+            case PPM_SOLID_ANGLE:
+            {
+                shapes::SphericalTriangle<float> st = shapes::SphericalTriangle<float>::create(vertex0, vertex1, vertex2, ray.origin);
+                const float rcpProb = st.solidAngleOfTriangle();
+                // if `rcpProb` is NAN then the triangle's solid angle was close to 0.0 
+                return rcpProb > numeric_limits<float>::min ? (1.0 / rcpProb) : numeric_limits<float>::max;
+            }
+            break;
+            case PPM_APPROX_PROJECTED_SOLID_ANGLE:
+            {
+                shapes::SphericalTriangle<float> st = shapes::SphericalTriangle<float>::create(vertex0, vertex1, vertex2, ray.origin);
+                const float pdf = st.projectedSolidAngleOfTriangle(ray.normalAtOrigin, ray.wasBSDFAtOrigin, L);
+                // if `pdf` is NAN then the triangle's projected solid angle was close to 0.0, if its close to INF then the triangle was very small
+                return pdf < numeric_limits<float>::max ? pdf : 0.0;
+            }
+            break;
+            default:
+                return 0.0;
+        }
+    }
+
+    template<class Aniso>
+    float32_t3 generate_and_pdf(NBL_REF_ARG(float32_t) pdf, NBL_REF_ARG(float32_t) newRayMaxT, NBL_CONST_REF_ARG(float32_t3) origin, NBL_CONST_REF_ARG(Aniso) interaction, bool isBSDF, float32_t3 xi, uint32_t objectID)
+    {
+        switch(polygonMethod)
+        {
+            case PPM_AREA:
+            {
+                const float32_t3 edge0 = vertex1 - vertex0;
+                const float32_t3 edge1 = vertex2 - vertex0;
+                const float sqrtU = nbl::hlsl::sqrt(xi.x);
+                float32_t3 pnt = vertex0 + edge0 * (1.0 - sqrtU) + edge1 * sqrtU * xi.y;
+                float32_t3 L = pnt - origin;
+                
+                const float distanceSq = nbl::hlsl::dot(L,L);
+                const float rcpDistance = 1.0 / nbl::hlsl::sqrt(distanceSq);
+                L *= rcpDistance;
+                
+                pdf = distanceSq / nbl::hlsl::abs(nbl::hlsl::dot(nbl::hlsl::cross(edge0, edge1) * 0.5f, L));
+                newRayMaxT = 1.0 / rcpDistance;
+                return L;
+            }
+            break;
+            case PPM_SOLID_ANGLE:
+            {
+                float rcpPdf;
+
+                shapes::SphericalTriangle<float> st = shapes::SphericalTriangle<float>::create(vertex0, vertex1, vertex2, ray.origin);
+                sampling::SphericalTriangle<float> sst = sampling::SphericalTriangle<float>::create(st);
+
+                const float32_t3 L = sst.generate(rcpPdf, xi.xy);
+
+                pdf = rcpPdf > numeric_limits<float>::min ? (1.0 / rcpPdf) : 0.0;
+
+                const float32_t3 N = getNormalTimesArea();
+                newRayMaxT = nbl::hlsl::dot(N, vertex0 - origin) / nbl::hlsl::dot(N, L);
+                return L;
+            }
+            break;
+            case PPM_APPROX_PROJECTED_SOLID_ANGLE:
+            {
+                float rcpPdf;
+
+                shapes::SphericalTriangle<float> st = shapes::SphericalTriangle<float>::create(vertex0, vertex1, vertex2, ray.origin);
+                sampling::ProjectedSphericalTriangle<float> sst = sampling::ProjectedSphericalTriangle<float>::create(st);
+            
+                const float32_t3 L = sst.generate(rcpPdf, interaction.N, isBSDF, xi.xy);
+
+                pdf = rcpPdf > numeric_limits<float>::min ? (1.0 / rcpPdf) : 0.0;
+
+                const float32_t3 N = getNormalTimesArea();
+                newRayMaxT = nbl::hlsl::dot(N, vertex0 - origin) / nbl::hlsl::dot(N, L);
+                return L;
+            }
+            break;
+            default:
+                return (float32_t3)0.0;
+        }
+    }
+
     NBL_CONSTEXPR_STATIC_INLINE uint32_t ObjSize = 10;
 
     float32_t3 vertex0;
     float32_t3 vertex1;
     float32_t3 vertex2;
     uint32_t bsdfLightIDs;
+    PTPolygonMethod polygonMethod;
 };
 
 template<>
@@ -196,6 +301,7 @@ struct Shape<PST_RECTANGLE>
         retval.edge0 = edge0;
         retval.edge1 = edge1;
         retval.bsdfLightIDs = spirv::bitFieldInsert<uint32_t>(bsdfID, lightID, 16, 16);
+        retval.polygonMethod = PPM_SOLID_ANGLE;
         return retval;
     }
 
@@ -238,6 +344,7 @@ struct Shape<PST_RECTANGLE>
     float32_t3 edge0;
     float32_t3 edge1;
     uint32_t bsdfLightIDs;
+    PTPolygonMethod polygonMethod;
 };
 
 }
