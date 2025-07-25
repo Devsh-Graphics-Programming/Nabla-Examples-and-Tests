25f1cd44e4f87a5ee9455bcf750fbcc1a1fc7ec5
kevyuu <kevin.kayu@gmail.com>
2025-07-25 17:34:35 +0700

    Fix example 71

--- 71_RayTracingPipeline/app_resources/raytrace.rchit.hlsl
+++ 71_RayTracingPipeline/app_resources/raytrace.rchit.hlsl
@@ -1,89 +1,87 @@
 #include "common.hlsl"
 
+#include "nbl/builtin/hlsl/bda/__ptr.hlsl"
+
 [[vk::push_constant]] SPushConstants pc;
 
-float32_t3 fetchVertexNormal(int instID, int primID, STriangleGeomInfo geom, float2 bary)
+float3 calculateNormals(int primID, STriangleGeomInfo geom, float2 bary)
 {
-    uint idxOffset = primID * 3;
-    
     const uint indexType = geom.indexType;
-    const uint vertexStride = geom.vertexStride;
-    
-    const uint32_t objType = geom.objType;
+    const uint normalType = geom.normalType;
+
+    const uint64_t vertexBufferAddress = geom.vertexBufferAddress;
     const uint64_t indexBufferAddress = geom.indexBufferAddress;
-    
-    uint i0, i1, i2;
-    switch (indexType)
+    const uint64_t normalBufferAddress = geom.normalBufferAddress;
+
+    uint32_t3 indices;
+    if (indexBufferAddress == 0)
     {
-        case 0: // EIT_16BIT
+        indices[0] = primID * 3;
+        indices[1] = indices[0] + 1;
+        indices[2] = indices[0] + 2;
+    }
+    else {
+        switch (indexType)
         {
-            i0 = uint32_t(vk::RawBufferLoad < uint16_t > (indexBufferAddress + (idxOffset + 0) * sizeof(uint16_t), 2u));
-            i1 = uint32_t(vk::RawBufferLoad < uint16_t > (indexBufferAddress + (idxOffset + 1) * sizeof(uint16_t), 2u));
-            i2 = uint32_t(vk::RawBufferLoad < uint16_t > (indexBufferAddress + (idxOffset + 2) * sizeof(uint16_t), 2u));
-        }
-        break;
-        case 1: // EIT_32BIT
-        {
-            i0 = vk::RawBufferLoad < uint32_t > (indexBufferAddress + (idxOffset + 0) * sizeof(uint32_t));
-            i1 = vk::RawBufferLoad < uint32_t > (indexBufferAddress + (idxOffset + 1) * sizeof(uint32_t));
-            i2 = vk::RawBufferLoad < uint32_t > (indexBufferAddress + (idxOffset + 2) * sizeof(uint32_t));
-        }
-        break;
-        default: // EIT_NONE
-        {
-            i0 = idxOffset;
-            i1 = idxOffset + 1;
-            i2 = idxOffset + 2;
+            case 0: // EIT_16BIT
+                indices = uint32_t3((nbl::hlsl::bda::__ptr<uint16_t3>::create(indexBufferAddress)+primID).deref().load());
+                break;
+            case 1: // EIT_32BIT
+                indices = uint32_t3((nbl::hlsl::bda::__ptr<uint32_t3>::create(indexBufferAddress)+primID).deref().load());
+                break;
         }
     }
 
-    const uint64_t normalVertexBufferAddress = geom.normalBufferAddress;
-    float3 n0, n1, n2;
+    if (normalBufferAddress == 0 || normalType == NT_UNKNOWN)
+    {
+        float3 v0 = vk::RawBufferLoad<float3>(vertexBufferAddress + indices[0] * 12);
+        float3 v1 = vk::RawBufferLoad<float3>(vertexBufferAddress + indices[1] * 12);
+        float3 v2 = vk::RawBufferLoad<float3>(vertexBufferAddress + indices[2] * 12);
+
+        return normalize(cross(v2 - v0, v1 - v0));
+    }
 
     float3 n0, n1, n2;
-    switch (objType)
+    switch (normalType)
     {
-        case OT_CUBE:
-        case OT_SPHERE:
-        case OT_RECTANGLE:
-        case OT_CYLINDER:
-        //case OT_ARROW:
-        case OT_CONE:
+        case NT_R8G8B8A8_SNORM:
         {
-            // TODO: document why the alignment is 2 here and nowhere else? isnt the `vertexStride` aligned to more than 2 anyway?
-            uint32_t v0 = vk::RawBufferLoad<uint32_t>(normalBufferAddress + i0 * 4);
-            uint32_t v1 = vk::RawBufferLoad<uint32_t>(normalBufferAddress + i1 * 4);
-            uint32_t v2 = vk::RawBufferLoad<uint32_t>(normalBufferAddress + i2 * 4);
+            uint32_t v0 = vk::RawBufferLoad<uint32_t>(normalBufferAddress + indices[0] * 4);
+            uint32_t v1 = vk::RawBufferLoad<uint32_t>(normalBufferAddress + indices[1] * 4);
+            uint32_t v2 = vk::RawBufferLoad<uint32_t>(normalBufferAddress + indices[2] * 4);
 
             n0 = normalize(nbl::hlsl::spirv::unpackSnorm4x8(v0).xyz);
             n1 = normalize(nbl::hlsl::spirv::unpackSnorm4x8(v1).xyz);
             n2 = normalize(nbl::hlsl::spirv::unpackSnorm4x8(v2).xyz);
         }
         break;
-        case OT_ICOSPHERE:
-        default:
+        case NT_R32G32B32_SFLOAT:
         {
-            n0 = normalize(vk::RawBufferLoad<float3>(normalBufferAddress + i0 * 12));
-            n1 = normalize(vk::RawBufferLoad<float3>(normalBufferAddress + i1 * 12));
-            n2 = normalize(vk::RawBufferLoad<float3>(normalBufferAddress + i2 * 12));
+            n0 = normalize(vk::RawBufferLoad<float3>(normalBufferAddress + indices[0] * 12));
+            n1 = normalize(vk::RawBufferLoad<float3>(normalBufferAddress + indices[1] * 12));
+            n2 = normalize(vk::RawBufferLoad<float3>(normalBufferAddress + indices[2] * 12));
         }
+        break;
     }
 
     float3 barycentrics = float3(0.0, bary);
-    barycentrics.x = 1.0 - barycentrics.y - barycentrics.z;
-    return normalize(barycentrics.x * n0 + barycentrics.y * n1 + barycentrics.z * n2);
+    barycentrics.x = 1.0 - barycentrics.y - barycentrics.z;        
+
+    return barycentrics.x * n0 + barycentrics.y * n1 + barycentrics.z * n2;
 }
+
 
 [shader("closesthit")]
 void main(inout PrimaryPayload payload, in BuiltInTriangleIntersectionAttributes attribs)
 {
-    const int instID = InstanceID();
     const int primID = PrimitiveIndex();
-    const STriangleGeomInfo geom = vk::RawBufferLoad < STriangleGeomInfo > (pc.triangleGeomInfoBuffer + instID * sizeof(STriangleGeomInfo));
-    const float32_t3 vertexNormal = fetchVertexNormal(instID, primID, geom, attribs.barycentrics);
+    const int instanceCustomIndex = InstanceIndex();
+    const int geometryIndex = GeometryIndex();
+    const STriangleGeomInfo geom = vk::RawBufferLoad < STriangleGeomInfo > (pc.triangleGeomInfoBuffer + (instanceCustomIndex + geometryIndex) * sizeof(STriangleGeomInfo));
+    const float32_t3 vertexNormal = calculateNormals(primID, geom, attribs.barycentrics);
     const float32_t3 worldNormal = normalize(mul(vertexNormal, WorldToObject3x4()).xyz);
 
-    payload.materialId = MaterialId::createTriangle(instID);
+    payload.materialId = MaterialId::createTriangle(instanceCustomIndex);
 
     payload.worldNormal = worldNormal;
     payload.rayDistance = RayTCurrent();
--- 71_RayTracingPipeline/include/common.hpp
+++ 71_RayTracingPipeline/include/common.hpp
@@ -22,32 +22,8 @@ using namespace nbl::examples;
 namespace nbl::scene
 {
 
-enum ObjectType : uint8_t
-{
-	OT_CUBE,
-	OT_SPHERE,
-	OT_CYLINDER,
-	OT_RECTANGLE,
-	OT_DISK,
-	OT_ARROW,
-	OT_CONE,
-	OT_ICOSPHERE,
-
-	OT_COUNT,
-	OT_UNKNOWN = std::numeric_limits<uint8_t>::max()
-};
-
-static constexpr uint32_t s_smoothNormals[OT_COUNT] = { 0, 1, 1, 0, 0, 1, 1, 1 };
-
-struct ObjectMeta
-{
-	ObjectType type = OT_UNKNOWN;
-	std::string_view name = "Unknown";
-};
-
 struct ReferenceObjectCpu
 {
-	ObjectMeta meta;
 	core::smart_refctd_ptr<ICPUPolygonGeometry> data;
 	Material material;
   core::matrix3x4SIMD transform;
