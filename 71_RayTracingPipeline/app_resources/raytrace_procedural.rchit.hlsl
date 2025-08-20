e1972c7eb39fb0a0b970e03cc98126204ac97313
kevyuu <kevin.kayu@gmail.com>
2025-08-20 17:39:41 +0700

    Convert more PrimitiveIndex() into inline spirv

--- 71_RayTracingPipeline/app_resources/raytrace_procedural.rchit.hlsl
+++ 71_RayTracingPipeline/app_resources/raytrace_procedural.rchit.hlsl
@@ -1,5 +1,6 @@
 #include "common.hlsl"
 
+#include "nbl/builtin/hlsl/spirv_intrinsics/core.hlsl"
 #include "nbl/builtin/hlsl/spirv_intrinsics/raytracing.hlsl"
 using namespace nbl::hlsl;
 
@@ -11,7 +12,7 @@ void main(inout PrimaryPayload payload, in ProceduralHitAttribute attrib)
     const float32_t3 worldPosition = spirv::WorldRayOriginKHR + spirv::WorldRayDirectionKHR * spirv::RayTmaxKHR;
     const float32_t3 worldNormal = normalize(worldPosition - attrib.center);
 
-    payload.materialId = MaterialId::createProcedural(PrimitiveIndex()); // we use negative value to indicate that this is procedural
+    payload.materialId = MaterialId::createProcedural(spirv::PrimitiveId); // we use negative value to indicate that this is procedural
 
     payload.worldNormal = worldNormal;
     payload.rayDistance = spirv::RayTmaxKHR;
