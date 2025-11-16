bd5392da633d446305e023341c9b95d29464d686
deprilula28 <deprilula28@gmail.com>
2025-11-16 00:23:29 -0300

    Work on cooperative binary search

---
+++ 72_CooperativeBinarySearch/app_resources/common.h
@@ -0,0 +1,19 @@
+#ifndef _COOPERATIVE_BINARY_SEARCH_HLSL_INCLUDED_
+#define _COOPERATIVE_BINARY_SEARCH_HLSL_INCLUDED_
+
+#include <nbl/builtin/hlsl/cpp_compat/basic.h>
+#include <nbl/builtin/hlsl/cpp_compat/matrix.hlsl>
+
+using namespace nbl::hlsl;
+namespace nbl {
+namespace hlsl {
+
+struct PushConstants
+{
+	uint32_t EntityCount;
+};
+
+};
+};
+
+#endif // _COOPERATIVE_BINARY_SEARCH_HLSL_INCLUDED_
