668884349e3a69346ac5212862b191daa42a695c
keptsecret <sorchon@gmail.com>
2025-11-19 14:44:55 +0700

    refactor box muller usage

--- 31_HLSLPathTracer/app_resources/hlsl/ray_gen.hlsl
+++ 31_HLSLPathTracer/app_resources/hlsl/ray_gen.hlsl
@@ -48,7 +48,9 @@ struct Basic
         vector2_type remappedRand = randVec.xy;
         remappedRand.x *= 1.0 - truncation;
         remappedRand.x += truncation;
-        tmp.xy += pixOffsetParam * nbl::hlsl::boxMullerTransform<scalar_type>(remappedRand, 1.5);
+        nbl::hlsl::sampling::BoxMullerTransform<scalar_type> boxMuller;
+        boxMuller.stddev = 1.5;
+        tmp.xy += pixOffsetParam * boxMuller(remappedRand);
         // for depth of field we could do another stochastic point-pick
         tmp = nbl::hlsl::mul(invMVP, tmp);
         ray.direction = nbl::hlsl::normalize(tmp.xyz / tmp.w - camPos);
