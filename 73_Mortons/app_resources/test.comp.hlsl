ba6641f6de9e107c923d3999cd7f52a8774797b7
kevyuu <kevin.kayu@gmail.com>
2025-12-01 18:20:31 +0700

    Use 1, 1, 1 workgroup dimension

--- 73_Mortons/app_resources/test.comp.hlsl
+++ 73_Mortons/app_resources/test.comp.hlsl
@@ -3,14 +3,15 @@
 //// For conditions of distribution and use, see copyright notice in nabla.h
 
 #include "testCommon.hlsl"
+#include "nbl/builtin/hlsl/glsl_compat/core.hlsl"
 
 [[vk::binding(0, 0)]] RWStructuredBuffer<InputTestValues> inputTestValues;
 [[vk::binding(1, 0)]] RWStructuredBuffer<TestValues> outputTestValues;
 
-[numthreads(256, 1, 1)]
+[numthreads(1, 1, 1)]
 [shader("compute")]
 void main(uint3 invocationID : SV_DispatchThreadID)
 {
-    if (invocationID.x == 0)
-        fillTestValues(inputTestValues[0], outputTestValues[0]);
+    uint32_t testID = glsl::gl_GlobalInvocationID().x;
+    fillTestValues(inputTestValues[testID], outputTestValues[testID]);
 }
