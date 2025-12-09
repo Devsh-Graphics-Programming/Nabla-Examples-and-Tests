6692311fddaf527dad42abe170394eb85ad4f5ae
kevyuu <kevin.kayu@gmail.com>
2025-12-09 23:59:46 +0700

    Delete fillSecondTestValues

--- 14_Mortons/app_resources/common.hlsl
+++ 14_Mortons/app_resources/common.hlsl
@@ -244,50 +244,6 @@ struct TestValues
 	morton::code<true, fullBits_4, 4, emulated_uint64_t>  mortonSignedRightShift_emulated_4;
 
 	
-	/*
-	void fillSecondTestValues(NBL_CONST_REF_ARG(InputTestValues) input)
-	{
-		uint64_t2 Vec2A = { input.coordX, input.coordY };
-		uint64_t2 Vec2B = { input.coordZ, input.coordW };
-
-		uint64_t3 Vec3A = { input.coordX, input.coordY, input.coordZ };
-		uint64_t3 Vec3B = { input.coordY, input.coordZ, input.coordW };
-
-		uint64_t4 Vec4A = { input.coordX, input.coordY, input.coordZ, input.coordW };
-		uint64_t4 Vec4B = { input.coordY, input.coordZ, input.coordW, input.coordX };
-
-		int64_t2 Vec2ASigned = int64_t2(Vec2A);
-		int64_t2 Vec2BSigned = int64_t2(Vec2B);
-
-		int64_t3 Vec3ASigned = int64_t3(Vec3A);
-		int64_t3 Vec3BSigned = int64_t3(Vec3B);
-
-		int64_t4 Vec4ASigned = int64_t4(Vec4A);
-		int64_t4 Vec4BSigned = int64_t4(Vec4B);
-
-		morton::code<false, fullBits_4, 4, emulated_uint64_t> morton_emulated_4A = morton::code<false, fullBits_4, 4, emulated_uint64_t>::create(Vec4A);
-		morton::code<true, fullBits_2, 2, emulated_uint64_t> morton_emulated_2_signed = morton::code<true, fullBits_2, 2, emulated_uint64_t>::create(Vec2ASigned);
-		morton::code<true, fullBits_3, 3, emulated_uint64_t> morton_emulated_3_signed = morton::code<true, fullBits_3, 3, emulated_uint64_t>::create(Vec3ASigned);
-		morton::code<true, fullBits_4, 4, emulated_uint64_t> morton_emulated_4_signed = morton::code<true, fullBits_4, 4, emulated_uint64_t>::create(Vec4ASigned);
-
-		output.mortonEqual_emulated_4 = uint32_t4(morton_emulated_4A.equal<false>(uint16_t4(Vec4B)));
-		
-		output.mortonUnsignedLess_emulated_4 = uint32_t4(morton_emulated_4A.lessThan<false>(uint16_t4(Vec4B)));
-		
-		mortonSignedLess_emulated_2 = uint32_t2(morton_emulated_2_signed.lessThan<false>(int32_t2(Vec2BSigned))); 
-		mortonSignedLess_emulated_3 = uint32_t3(morton_emulated_3_signed.lessThan<false>(int32_t3(Vec3BSigned))); 
-		mortonSignedLess_emulated_4 = uint32_t4(morton_emulated_4_signed.lessThan<false>(int16_t4(Vec4BSigned))); 
-
-		uint16_t castedShift = uint16_t(input.shift);
-
-		arithmetic_right_shift_operator<morton::code<true, fullBits_2, 2, emulated_uint64_t> > rightShiftSignedEmulated2;
-		mortonSignedRightShift_emulated_2 = rightShiftSignedEmulated2(morton_emulated_2_signed, castedShift); 
-		arithmetic_right_shift_operator<morton::code<true, fullBits_3, 3, emulated_uint64_t> > rightShiftSignedEmulated3;
-		mortonSignedRightShift_emulated_3 = rightShiftSignedEmulated3(morton_emulated_3_signed, castedShift); 
-		arithmetic_right_shift_operator<morton::code<true, fullBits_4, 4, emulated_uint64_t> > rightShiftSignedEmulated4;
-		mortonSignedRightShift_emulated_4 = rightShiftSignedEmulated4(morton_emulated_4_signed, castedShift); 
-	}
-	*/
 };
 
 #endif
