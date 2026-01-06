b70d47266041dcdd848a7dd11e74fee15ab7c9d5
keptsecret <sorchon@gmail.com>
2026-01-06 16:26:12 +0700

    setup quaternion unit test + some basic tests

---
+++ 59_QuaternionTests/CQuaternionTester.h
@@ -0,0 +1,95 @@
+#ifndef _NBL_EXAMPLES_TESTS_59_QUATERNION_TESTER_INCLUDED_
+#define _NBL_EXAMPLES_TESTS_59_QUATERNION_TESTER_INCLUDED_
+
+
+#include <glm/detail/type_quat.hpp>
+#include <glm/ext/quaternion_trigonometric.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/euler_angles.hpp>
+
+#include "nbl/examples/examples.hpp"
+#include "app_resources/common.hlsl"
+#include "nbl/examples/Tester/ITester.h"
+
+using namespace nbl;
+
+class CQuaternionTester final : public ITester<QuaternionInputTestValues, QuaternionTestValues, QuaternionTestExecutor>
+{
+    using base_t = ITester<QuaternionInputTestValues, QuaternionTestValues, QuaternionTestExecutor>;
+
+public:
+    CQuaternionTester(const uint32_t testBatchCount)
+        : base_t(testBatchCount) {};
+
+private:
+    QuaternionInputTestValues generateInputTestValues() override
+    {
+        std::uniform_real_distribution<float> realDistribution(-100.0f, 100.0f);
+        std::uniform_real_distribution<float> realDistributionSmall(1.0f, 4.0f);
+        std::uniform_int_distribution<int> intDistribution(-100, 100);
+        std::uniform_int_distribution<int> coinFlipDistribution(0, 1);
+
+        QuaternionInputTestValues testInput;
+        testInput.axis = float32_t3(realDistribution(getRandomEngine()), realDistribution(getRandomEngine()), realDistribution(getRandomEngine()));
+        testInput.angle = realDistribution(getRandomEngine());
+        testInput.quat0 = math::quaternion<float>::create(float32_t3(realDistribution(getRandomEngine()), realDistribution(getRandomEngine()), realDistribution(getRandomEngine())), realDistribution(getRandomEngine()));
+        testInput.quat0 = hlsl::normalize(testInput.quat0);
+        testInput.quat1 = math::quaternion<float>::create(float32_t3(realDistribution(getRandomEngine()), realDistribution(getRandomEngine()), realDistribution(getRandomEngine())), realDistribution(getRandomEngine()));
+        testInput.quat1 = hlsl::normalize(testInput.quat1);
+        testInput.pitch = realDistribution(getRandomEngine());
+        testInput.yaw = realDistribution(getRandomEngine());
+        testInput.roll = realDistribution(getRandomEngine());
+
+        return testInput;
+    }
+
+    QuaternionTestValues determineExpectedResults(const QuaternionInputTestValues& testInput) override
+    {
+        // use std library functions to determine expected test values, the output of functions from tgmath.hlsl will be verified against these values
+        QuaternionTestValues expected;
+        {
+            const auto glmquat = glm::angleAxis(testInput.angle, testInput.axis);
+            expected.quatFromAngleAxis.data.x = glmquat.data.data[0];
+            expected.quatFromAngleAxis.data.y = glmquat.data.data[1];
+            expected.quatFromAngleAxis.data.z = glmquat.data.data[2];
+            expected.quatFromAngleAxis.data.w = glmquat.data.data[3];
+        }
+        {
+            const auto rotmat = glm::yawPitchRoll(testInput.yaw, testInput.pitch, testInput.roll);
+            const auto glmquat = glm::quat_cast(rotmat);
+            expected.quatFromEulerAngles.data.x = glmquat.data.data[0];
+            expected.quatFromEulerAngles.data.y = glmquat.data.data[1];
+            expected.quatFromEulerAngles.data.z = glmquat.data.data[2];
+            expected.quatFromEulerAngles.data.w = glmquat.data.data[3];
+        }
+        {
+            const auto glmquat = glm::quat(testInput.quat0.data.w, testInput.quat0.data.x, testInput.quat0.data.y, testInput.quat0.data.z);
+            const auto rotmat = glm::mat3_cast(glmquat);
+            expected.rotationMat[0] = rotmat[0];
+            expected.rotationMat[1] = rotmat[1];
+            expected.rotationMat[2] = rotmat[2];
+        }
+        {
+            const auto glmquat0 = glm::quat(testInput.quat0.data.w, testInput.quat0.data.x, testInput.quat0.data.y, testInput.quat0.data.z);
+            const auto glmquat1 = glm::quat(testInput.quat1.data.w, testInput.quat1.data.x, testInput.quat1.data.y, testInput.quat1.data.z);
+            const auto mult = glmquat0 * glmquat1;
+            expected.quatMult.data.x = mult.data.data[0];
+            expected.quatMult.data.y = mult.data.data[1];
+            expected.quatMult.data.z = mult.data.data[2];
+            expected.quatMult.data.w = mult.data.data[3];
+        }
+
+        return expected;
+    }
+
+    void verifyTestResults(const QuaternionTestValues& expectedTestValues, const QuaternionTestValues& testValues, const size_t testIteration, const uint32_t seed, TestType testType) override
+    {
+        verifyTestValue("create from axis angle", expectedTestValues.quatFromAngleAxis.data, testValues.quatFromAngleAxis.data, testIteration, seed, testType, 1e-2);
+        verifyTestValue("create from Euler angles", expectedTestValues.quatFromEulerAngles.data, testValues.quatFromEulerAngles.data, testIteration, seed, testType, 1e-2);
+        verifyTestValue("multiply quat", expectedTestValues.quatMult.data, testValues.quatMult.data, testIteration, seed, testType, 1e-2);
+
+        verifyTestValue("construct matrix", expectedTestValues.rotationMat, testValues.rotationMat, testIteration, seed, testType, 1e-2);
+    }
+};
+
+#endif
---
+++ 59_QuaternionTests/app_resources/common.hlsl
@@ -0,0 +1,74 @@
+//// Copyright (C) 2023-2024 - DevSH Graphics Programming Sp. z O.O.
+//// This file is part of the "Nabla Engine".
+//// For conditions of distribution and use, see copyright notice in nabla.h
+
+#ifndef _NBL_EXAMPLES_TESTS_59_QUATERNION_COMMON_INCLUDED_
+#define _NBL_EXAMPLES_TESTS_59_QUATERNION_COMMON_INCLUDED_
+
+// because DXC doesn't properly support `_Static_assert`
+// TODO: add a message, and move to macros.h or cpp_compat
+#define STATIC_ASSERT(...) { nbl::hlsl::conditional<__VA_ARGS__, int, void>::type a = 0; }
+
+#include <boost/preprocessor.hpp>
+
+#include <nbl/builtin/hlsl/cpp_compat.hlsl>
+#include <nbl/builtin/hlsl/type_traits.hlsl>
+
+#include <nbl/builtin/hlsl/cpp_compat/matrix.hlsl>
+#include <nbl/builtin/hlsl/cpp_compat/vector.hlsl>
+
+#include <nbl/builtin/hlsl/colorspace/encodeCIEXYZ.hlsl>
+#include <nbl/builtin/hlsl/colorspace/decodeCIEXYZ.hlsl>
+#include <nbl/builtin/hlsl/colorspace/EOTF.hlsl>
+#include <nbl/builtin/hlsl/colorspace/OETF.hlsl>
+
+#include <nbl/builtin/hlsl/random/xoroshiro.hlsl>
+
+#include <nbl/builtin/hlsl/mpl.hlsl>
+#include <nbl/builtin/hlsl/bit.hlsl>
+
+#include <nbl/builtin/hlsl/limits.hlsl>
+
+
+#include <nbl/builtin/hlsl/barycentric/utils.hlsl>
+#include <nbl/builtin/hlsl/member_test_macros.hlsl>
+#include <nbl/builtin/hlsl/device_capabilities_traits.hlsl>
+
+#include <nbl/builtin/hlsl/tgmath.hlsl>
+#include <nbl/builtin/hlsl/cpp_compat/intrinsics.hlsl>
+
+#include <nbl/builtin/hlsl/math/quaternions.hlsl>
+
+using namespace nbl::hlsl;
+struct QuaternionInputTestValues
+{
+    math::quaternion<float> quat0;
+    math::quaternion<float> quat1;
+    float32_t3 axis;
+    float angle;
+    float pitch;
+    float yaw;
+    float roll;
+    float32_t3x3 rotationMat;
+};
+
+struct QuaternionTestValues
+{
+    math::quaternion<float> quatFromAngleAxis;
+    math::quaternion<float> quatFromEulerAngles;
+    float32_t3x3 rotationMat;
+    math::quaternion<float> quatMult;
+};
+
+struct QuaternionTestExecutor
+{
+    void operator()(NBL_CONST_REF_ARG(QuaternionInputTestValues) input, NBL_REF_ARG(QuaternionTestValues) output)
+    {
+        output.quatFromAngleAxis = math::quaternion<float>::create(input.axis, input.angle);
+        output.quatFromEulerAngles = math::quaternion<float>::create(input.pitch, input.yaw, input.roll);
+        output.rotationMat = input.quat0.constructMatrix();
+        output.quatMult = input.quat0 * input.quat1;
+    }
+};
+
+#endif
