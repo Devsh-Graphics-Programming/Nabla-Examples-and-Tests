f466d64c96374cb9c80769c664c7aba99f228a69
Fletterio <fj.letterio@gmail.com>
2026-05-05 00:52:54 -0300

    Halfway added subgroup shared twiddles

--- XX_NewFFT/app_resources/shader.comp.hlsl
+++ XX_NewFFT/app_resources/shader.comp.hlsl
@@ -96,7 +96,7 @@ void main(uint32_t3 ID : SV_DispatchThreadID)
 	//adaptor_t sharedmemAdaptor;
 	//sharedmemAdaptor.accessor = sharedmemAccessor;
 
-	using ConstevalParameters = workgroup2::fft::ConstevalParameters<ElementsPerThread, SubgroupSizeLog2, WorkgroupSizeLog2, ShuffledChannelsPerRound, false, 0, float32_t>;
+	using ConstevalParameters = workgroup2::fft::ConstevalParameters<ElementsPerThread, SubgroupSizeLog2, WorkgroupSizeLog2, ShuffledChannelsPerRound, false, 0, true, float32_t>;
 	using FFT = workgroup2::FFT<false, ConstevalParameters>;
 	using IFFT = workgroup2::FFT<true, ConstevalParameters>;
 
